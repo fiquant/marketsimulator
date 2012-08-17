e446953208e7b63a6f3d01b9993794ddf8fd9b10
Anton Kolotaev <anton.kolotaev@gmail.com>
2012-08-17 18:18:26 +0000

    tabsize: 3 --> 4

--- marketsim/order.py
+++ marketsim/order.py
@@ -2,147 +2,146 @@ from marketsim import Side
 
 class OrderBase(object):
 
-   def __init__(self, volume):
-      self._volume = volume
-      self._cancelled = False
-      self._PnL = 0
-      self.on_matched = set()
+    def __init__(self, volume):
+        self._volume = volume
+        self._cancelled = False
+        self._PnL = 0
+        self.on_matched = set()
 
-   def __str__(self):
-      return type(self).__name__ + "(volume=" + str(self.volume) + ", P&L="+str(self.PnL)+")"
+    def __str__(self):
+        return type(self).__name__ + "(volume=" + str(self.volume) + ", P&L="+str(self.PnL)+")"
 
-   def __repr__(self):
-      return self.__str__()
+    def __repr__(self):
+        return self.__str__()
 
-   @property
-   def volume(self):
-      return self._volume
+    @property
+    def volume(self):
+        return self._volume
 
-   @property
-   def empty(self):
-      return self.volume == 0
+    @property
+    def empty(self):
+        return self.volume == 0
 
-   @property
-   def cancelled(self):
-      return self._cancelled
+    @property
+    def cancelled(self):
+        return self._cancelled
 
-   def cancel(self):
-      self._cancelled = True
+    def cancel(self):
+        self._cancelled = True
 
-   def notifyOnMatched(self, other, (price, volume)):
-      for x in self.on_matched:
-          x(self, other, (price, volume))
+    def notifyOnMatched(self, other, (price, volume)):
+        for x in self.on_matched:
+            x(self, other, (price, volume))
 
 
-   """
-   Called on order matching
-   other - other order
-   price - price at which the match was done
-   volume - volume of the match
-   """
-   def onMatchedWith(self, other, (price,volume)):
-      self._volume -= volume
-      self._PnL += volume * price
-      self.notifyOnMatched(other, (price, volume))
+    def onMatchedWith(self, other, (price,volume)):
+        """
+        Called on order matching
+        other - other order
+        price - price at which the match was done
+        volume - volume of the match
+        """
+        self._volume -= volume
+        self._PnL += volume * price
+        self.notifyOnMatched(other, (price, volume))
 
 
 class LimitOrderBase(OrderBase):
 
-   def __init__(self, price, volume):
-      OrderBase.__init__(self, volume)
-      self._price = price
+    def __init__(self, price, volume):
+        OrderBase.__init__(self, volume)
+        self._price = price
 
-   def __str__(self):
-      return type(self).__name__ + "(Price=" + str(self.price) + ", volume=" + str(self.volume) + ", P&L="+str(self.PnL)+")"
+    def __str__(self):
+        return type(self).__name__ + "(Price=" + str(self.price) + ", volume=" + str(self.volume) + ", P&L="+str(self.PnL)+")"
 
-   def processIn(self, orderBook):
-      orderBook.processLimitOrder(self)
+    def processIn(self, orderBook):
+        orderBook.processLimitOrder(self)
 
-   @property
-   def price(self):
-      return self._price
+    @property
+    def price(self):
+        return self._price
 
-   @price.setter
-   def price(self, value):
-      self._price = value
+    @price.setter
+    def price(self, value):
+        self._price = value
 
-   """
-   Matches the order with another one
-   Returns True iff this order is completely matched
-   """
-   def matchWith(self, other):
-      if other.canBeMatched(self):
-         v = min(self.volume, other.volume)
-         assert v > 0
-         p = self.price
-         self.onMatchedWith(other, (p,v))
-         other.onMatchedWith(self, (p,v))
-      return self.empty
+    """
+    Matches the order with another one
+    Returns True iff this order is completely matched
+    """
+    def matchWith(self, other):
+        if other.canBeMatched(self):
+            v = min(self.volume, other.volume)
+            assert v > 0
+            p = self.price
+            self.onMatchedWith(other, (p,v))
+            other.onMatchedWith(self, (p,v))
+        return self.empty
 
 class BuySideOrderBase(object):
 
-   # Returns P&L for the order
-   # negative for buy orders
-   @property
-   def PnL(self):
-      return -self._PnL
+    # Returns P&L for the order
+    # negative for buy orders
+    @property
+    def PnL(self):
+        return -self._PnL
 
-   side = Side.Buy
+    side = Side.Buy
 
 class SellSideOrderBase(object):
 
-   # Returns P&L for the order
-   # positive for sell orders
-   @property
-   def PnL(self):
-      return +self._PnL
+    # Returns P&L for the order
+    # positive for sell orders
+    @property
+    def PnL(self):
+        return +self._PnL
 
-   side = Side.Sell
+    side = Side.Sell
 
 class MarketOrderBase(OrderBase):
 
-   def __init__(self, volume):
-      OrderBase.__init__(self, volume)
+    def __init__(self, volume):
+        OrderBase.__init__(self, volume)
 
-   def processIn(self, orderBook):
-      orderBook.processMarketOrder(self)
+    def processIn(self, orderBook):
+        orderBook.processMarketOrder(self)
 
-   def canBeMatched(self, other):
-       assert other.side == Side.opposite(self.side)
-       return True
+    def canBeMatched(self, other):
+        assert other.side == Side.opposite(self.side)
+        return True
 
 class MarketOrderBuy(MarketOrderBase, BuySideOrderBase):
 
-   def __init__(self, volume):
-      MarketOrderBase.__init__(self, volume)
+    def __init__(self, volume):
+        MarketOrderBase.__init__(self, volume)
 
 class MarketOrderSell(MarketOrderBase, SellSideOrderBase):
 
-   def __init__(self, volume):
-      MarketOrderBase.__init__(self, volume)
+    def __init__(self, volume):
+        MarketOrderBase.__init__(self, volume)
 
 def MarketOrderT(side):
     return MarketOrderSell if side==Side.Sell else MarketOrderBuy
 
 class LimitOrderBuy(LimitOrderBase, BuySideOrderBase):
 
-   def __init__(self, price, volume):
-      LimitOrderBase.__init__(self, price, volume)
+    def __init__(self, price, volume):
+        LimitOrderBase.__init__(self, price, volume)
 
-   def canBeMatched(self, other):
-      assert other.side == Side.Sell
-      return self.price >= other.price
+    def canBeMatched(self, other):
+        assert other.side == Side.Sell
+        return self.price >= other.price
 
 
 class LimitOrderSell(LimitOrderBase, SellSideOrderBase):
 
-   def __init__(self, price, volume):
-      LimitOrderBase.__init__(self, price, volume)
+    def __init__(self, price, volume):
+        LimitOrderBase.__init__(self, price, volume)
 
-   def canBeMatched(self, other):
-      assert other.side == Side.Buy
-      return self.price <= other.price
+    def canBeMatched(self, other):
+        assert other.side == Side.Buy
+        return self.price <= other.price
 
 def LimitOrderT(side):
     return LimitOrderSell if side==Side.Sell else LimitOrderBuy
-
--- marketsim/test.py
+++ marketsim/test.py
@@ -1,27 +1,27 @@
 class HistoryCheckerBase(object):
-   def __init__(self):
-      self._history = []
-      self._delta = []
+    def __init__(self):
+        self._history = []
+        self._delta = []
 
-   def append(self, value):
-      self._delta.append(value)
+    def append(self, value):
+        self._delta.append(value)
 
-   def checkDelta(self, other):
-      res = other == self._delta
-      self._history.extend(self._delta)
-      self._delta = []
-      return res
+    def checkDelta(self, other):
+        res = other == self._delta
+        self._history.extend(self._delta)
+        self._delta = []
+        return res
 
-   @property
-   def history(self):
-      return self._history + self._delta
+    @property
+    def history(self):
+        return self._history + self._delta
 
 class OrderQueueHistoryChecker(HistoryCheckerBase):
 
-   def append(self, queue, best):
-      HistoryCheckerBase.append(self, (best.price, best.volume) if best else None)
+    def append(self, queue, best):
+        HistoryCheckerBase.append(self, (best.price, best.volume) if best else None)
 
 class TraderHistoryChecker(HistoryCheckerBase):
 
-   def append(self, trader):
-      HistoryCheckerBase.append(self, (trader.PnL, trader.amount))
+    def append(self, trader):
+        HistoryCheckerBase.append(self, (trader.PnL, trader.amount))
