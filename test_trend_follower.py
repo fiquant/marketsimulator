bff69d62998ec8a5ba547896e4f71a24077603a9
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2012-08-21 12:34:17 +0000

    TrendFollower added
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@263 c58600cb-c4ea-42d0-b18c-69d24e00f01c

---
+++ test_trend_follower.py
@@ -0,0 +1,44 @@
+from marketsim.order_queue import OrderBook
+from marketsim.trader import TrendFollower
+from marketsim.order import LimitOrderSell, LimitOrderBuy
+from marketsim.scheduler import world
+
+book = OrderBook()
+trader = TrendFollower(book, creationIntervalDistr=lambda: 1, volumeDistr=lambda: 1)
+
+for x in range(90, 100):
+    book.process(LimitOrderBuy(x, 1))
+    
+for x in range(101, 111):
+    book.process(LimitOrderSell(x, 1))
+    
+assert trader.amount == 0
+assert trader.PnL == 0
+
+world.workTill(0.9)
+
+book.process(LimitOrderBuy(101,2))
+
+assert book.asks.best.price == 102
+assert book.price == 101.5
+
+world.workTill(1.1)
+
+assert book.asks.best.price == 103
+assert book.price == 102
+
+assert trader.amount == 1
+assert trader.PnL == -102
+
+book.process(LimitOrderSell(95,10))
+assert book.asks.best.price == 95
+assert book.bids.best.price == 94
+    
+world.workTill(2.1)
+
+assert book.bids.best.price == 93
+assert trader.amount == 0
+assert trader.PnL == -102 +94
+
+
+world.reset()
