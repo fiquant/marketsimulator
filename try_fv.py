8c89a87fa02c60520c9925abbc49b0a9b21238f0
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2012-11-09 13:22:10 +0000

    virtual market order
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@296 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- try_fv.py
+++ try_fv.py
@@ -3,7 +3,8 @@ from marketsim.scheduler import Scheduler
 from marketsim.order_queue import OrderBook
 from marketsim.trader import LiquidityProvider, FVTrader
 from marketsim import Side
-from marketsim.indicator import AssetPrice, OnEveryDt, EWMA, TraderEfficiency, PnL
+from marketsim.indicator import AssetPrice, OnEveryDt, EWMA, TraderEfficiency, PnL, VolumeTraded
+from marketsim.order import VirtualMarketOrderT
 
 world = Scheduler()
 
@@ -19,17 +20,36 @@ def avg(source, alpha=0.15):
 
 price_graph.addTimeSerie(avg(assetPrice))
 
-seller_A = LiquidityProvider(book_A, Side.Sell)
-buyer_A = LiquidityProvider(book_A, Side.Buy)
-trader = FVTrader(book_A, fundamentalValue=lambda: 200.)
+seller_A = LiquidityProvider(book_A, Side.Sell, volumeDistr=lambda: 20)
+buyer_A = LiquidityProvider(book_A, Side.Buy, volumeDistr=lambda: 20)
 
-trader.label = "fv"
-trader.efficiency = TraderEfficiency([trader.on_traded], trader)
+trader_200 = FVTrader(book_A, fundamentalValue=lambda: 200., volumeDistr=lambda: 5)
+trader_150 = FVTrader(book_A, fundamentalValue=lambda: 150., volumeDistr=lambda: 2)
+
+trader_200.label = "t200"
+trader_150.label = "t150"
+
+virtual_160 = FVTrader(book_A, fundamentalValue=lambda: 160., orderFactory=VirtualMarketOrderT, volumeDistr=lambda: 1)
+virtual_170 = FVTrader(book_A, fundamentalValue=lambda: 170., orderFactory=VirtualMarketOrderT, volumeDistr=lambda: 1)
+virtual_180 = FVTrader(book_A, fundamentalValue=lambda: 180., orderFactory=VirtualMarketOrderT, volumeDistr=lambda: 1)
+
+virtual_160.label = "v160"
+virtual_170.label = "v170"
+virtual_180.label = "v180"
+
+def efficiency(trader):
+    return TraderEfficiency([trader.on_traded], trader)
+
+def addToGraph(graph, traders):
+    for t in traders:
+        eff_graph.addTimeSerie(efficiency(t))
+        eff_graph.addTimeSerie(PnL(t))
+        eff_graph.addTimeSerie(VolumeTraded(t))
+        
 
 eff_graph = Graph("efficiency")
-eff_graph.addTimeSerie(trader.efficiency)
-eff_graph.addTimeSerie(PnL(trader))
+addToGraph(eff_graph, [trader_150, trader_200, virtual_160, virtual_170, virtual_180])
 
-world.workTill(500)
+world.workTill(1500)
 
 showGraphs("fv_trader", [price_graph, eff_graph])
