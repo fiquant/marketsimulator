0f102f81a228a5e2e9621c4c328bc7946924d02d
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2012-12-11 10:36:46 +0000

    wrapping constant and random functions
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@340 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- marketsim/mathutils/__init__.py
+++ marketsim/mathutils/__init__.py
@@ -1,1 +1,13 @@
 from _average import ewma
+import rnd
+
+class constant(object):
+    
+    def __init__(self, x):
+        self._value = x
+        
+    def __call__(self, *args, **kwargs):
+        return self._value
+    
+    def __repr__(self):
+        return "constant("+self._value+")"
