6b9e7179747157e82410e99b508bb76e6360e324
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-03-01 19:48:48 +0000

    array and scalar fields are factored out
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@408 c58600cb-c4ea-42d0-b18c-69d24e00f01c

---
+++ server/static/js/fields/array.js
@@ -0,0 +1,38 @@
+/**
+ * Field of array type 
+ * @param {list<Field>} s -- array of fields (of scalar, array or object type)
+ */
+
+function ArrayValue(s) {
+	var self = this;
+	self.array = function () { return true; }
+	
+	self._storage = ko.observableArray(map(s, function (x,i) {
+						return new Property(i, x, true);
+					}));
+	
+	/**
+	 *	Elements of the array 
+	 */				
+	self.elements = ko.computed(function () {
+		return self._storage();
+	})
+	
+	/**
+	 *  For the moment we consider all arrays as value types  
+	 */
+	self.isReference = function () { return false; }
+
+	/**
+	 *  Returns true iff any child has an error 
+	 */
+	self.hasError = ko.computed(function () {
+		return any(self._storage(), function (x) { return x.val.hasError(); })
+	})
+
+	/**
+	 *  expanded representation of the array 
+	 */
+	self.expanded = self._storage;
+}
+
