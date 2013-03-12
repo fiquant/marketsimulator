dd4323c3ceb2027e8ca506fce2e0407bdfe97694
Anton Kolotaev <anton.kolotaev@gmail.com>
2013-03-12 20:44:51 +0000

    orders with expiration exposed to the web interface

--- marketsim/order/__init__.py
+++ marketsim/order/__init__.py
@@ -2,7 +2,7 @@ from marketsim import Side, Event
 
 from _market import Market
 from _limit import Limit
-from _cancel import Cancel, LimitMarket
+from _cancel import Cancel, LimitMarket, WithExpiry, WithExpiryFactory
 from _virtual import VirtualMarket
 from _always_best import AlwaysBest        
 from _iceberg import iceberg, Iceberg
