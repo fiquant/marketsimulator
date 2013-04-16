9012b43e47148ec0c609e27f838548e5fae5edd9
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-04-16 11:32:20 +0000

    canceller added to samples
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@495 c58600cb-c4ea-42d0-b18c-69d24e00f01c

---
+++ samples/try_canceller.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append(r'..')
+
+from marketsim import (strategy, trader, orderbook, order, mathutils, Side,
+                       scheduler, observable, veusz, registry)
+
+from common import run 
+
+def Canceller(graph, world, books):
+
+    book_A = books['Asset A']
+
+    price_graph = graph("Price")
+     
+    assetPrice = observable.AskPrice(book_A)
+    
+    avg = observable.avg
+    
+    lp = trader.SASM(book_A, 
+                     strategy.LiquidityProviderSide(side = Side.Sell),
+                     "LiquidityProvider")
+
+    lp_B = trader.SASM(book_A, 
+                     strategy.LiquidityProviderSide(side = Side.Buy),
+                     "LiquidityProviderBuy")
+
+    lp_C = trader.SASM(book_A, 
+                       strategies = [
+                            strategy.LiquidityProviderSide(side = Side.Sell),
+                            strategy.Canceller()
+                        ],
+                       label = "LiquidityProviderWithCanceller")
+    
+    lp_A = trader.SASM(book_A, 
+                       strategy.LiquidityProviderSide(
+                            side = Side.Sell,
+                            orderFactoryT=order.WithExpiryFactory(
+                                expirationDistr=mathutils.constant(10))),
+                       "LiquidityProviderWithExpiry")
+    
+    fv_trader = trader.SASM(book_A, 
+                            strategy.FundamentalValue(fundamentalValue = mathutils.constant(1000)), 
+                            "fv_1000")
+    
+    price_graph += [assetPrice,
+                    avg(assetPrice)]
+    
+    amount_graph = graph("amount")
+    amount_graph += [observable.VolumeTraded(lp),
+                     observable.VolumeTraded(lp_C),
+                     observable.VolumeTraded(lp_A)]
+    
+    return [lp_A, lp, lp_C, lp_B, fv_trader], [price_graph, amount_graph]
+
+if __name__ == '__main__':    
+    run("canceller", Canceller)
