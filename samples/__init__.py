e4d4c908caf9941688aa8c2d20159e70bdce5989
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-04-16 08:51:31 +0000

    trend follower added to samples
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@492 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- samples/__init__.py
+++ samples/__init__.py
@@ -3,3 +3,4 @@ from try_dependency import Dependency
 from try_noise import Noise
 from try_registry import Complete
 from try_signal import Signal
+from try_trend_follower import TrendFollower
---
+++ samples/try_trend_follower.py
@@ -0,0 +1,60 @@
+import sys
+sys.path.append(r'..')
+
+from marketsim import (signal, strategy, trader, orderbook, order,
+                       scheduler, observable, veusz, mathutils)
+
+from common import run
+
+const = mathutils.constant
+
+def TrendFollower(graph, world, books):
+
+    book_A = books['Asset A']
+
+    price_graph = graph("Price")
+     
+    assetPrice = observable.Price(book_A)
+    
+    avg = observable.avg
+    
+    V = 1
+    
+    lp_A = trader.SASM(book_A, 
+                       strategy.LiquidityProvider(
+                            volumeDistr=const(V*3), 
+                            orderFactoryT=order.WithExpiryFactory(
+                                expirationDistr=const(100))),
+                       label="LiquidityProvider")
+    
+    linear_signal = signal.RandomWalk(initialValue=200, 
+                                      deltaDistr=const(-1), 
+                                      label="200-t")
+    
+    signal_trader = trader.SASM(book_A, 
+                                strategy.Signal(linear_signal, 
+                                                volumeDistr = const(V)), 
+                                "signal")
+    
+    alpha = 0.065
+    
+    trend_follower = trader.SASM(book_A, 
+                                 strategy.TrendFollower(
+                                    average=mathutils.ewma(alpha),
+                                    volumeDistr = const(V)),
+                                 label="trendfolllower")
+    
+    price_graph += [assetPrice,
+                    avg(assetPrice, alpha),
+                    linear_signal,
+                    observable.VolumeTraded(signal_trader), 
+                    observable.VolumeTraded(trend_follower),]
+    
+    eff_graph = graph("efficiency")
+    eff_graph += [observable.Efficiency(trend_follower),
+                  observable.PnL(trend_follower)]
+    
+    return [lp_A, signal_trader, trend_follower], [price_graph, eff_graph]
+
+if __name__ == '__main__':
+    run("trend_follower", TrendFollower)
