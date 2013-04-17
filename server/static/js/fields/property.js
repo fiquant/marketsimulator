7304172b4ce21474708246a0c0329edfb0f5b61a
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-04-17 12:33:42 +0000

    tab navigation to switch between entities of the same kind
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@499 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- server/static/js/fields/property.js
+++ server/static/js/fields/property.js
@@ -120,7 +120,7 @@ function Property(name, value, toplevel, parentArray) {
 	/**
 	 *  Returns true if the field is expanded at the moment 
 	 */
-	self.isExpanded = ko.observable(!toplevel && self.expandable.peek());
+	self.isExpanded = ko.observable(self.expandable.peek());
 	
 	/**
 	 *	Returns array of expanded field items if in expanded state 
