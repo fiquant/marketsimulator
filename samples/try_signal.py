a5d295b5564468b6c2155057e10b0f54a5b11214
Anton Kolotaev <anton.kolotaev@gmail.com>
2013-04-21 11:57:42 +0000

    implementing signal strategy via generic one

--- samples/try_signal.py
+++ samples/try_signal.py
@@ -19,22 +19,26 @@ def Signal(graph, world, books):
     avg = observable.avg
     
     lp_A = trader.SASM(book_A, 
-                       strategy.LiquidityProvider(volumeDistr=const(1)),
+                       strategy.LiquidityProvider(volumeDistr=const(2)),
                        "liquidity")
     
     linear_signal = signal.RandomWalk(initialValue=20, deltaDistr=const(-.1), label="20-0.1t")
     signal_trader = trader.SASM(book_A, strategy.Signal(linear_signal), "signal")
+    signal_ex_trader = trader.SASM(book_A, strategy.SignalEx(linear_signal), "signal_ex")
     
     price_graph += [assetPrice,
                     avg(assetPrice),
                     linear_signal,
-                    observable.VolumeTraded(signal_trader)]
+                    observable.VolumeTraded(signal_trader), 
+                    observable.VolumeTraded(signal_ex_trader)]
     
     eff_graph = graph("efficiency")
     eff_graph += [observable.Efficiency(signal_trader),
                   observable.PnL(signal_trader)]
+    eff_graph += [observable.Efficiency(signal_ex_trader),
+                  observable.PnL(signal_ex_trader)]
     
-    return [lp_A, signal_trader], [price_graph, eff_graph]
+    return [lp_A, signal_trader, signal_ex_trader], [price_graph, eff_graph]
 
 if __name__ == '__main__':
     run("signal_trader", Signal)
