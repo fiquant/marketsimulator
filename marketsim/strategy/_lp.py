6fd5122d35c5a488d3b21b7285d9f72b259dc6f5
Anton Kolotaev <anton.kolotaev@gmail.com>
2013-05-19 18:25:51 +0000

    LiquidityProvider2 added

--- marketsim/strategy/_lp.py
+++ marketsim/strategy/_lp.py
@@ -1,8 +1,70 @@
 import random
-from _basic import OneSide, Strategy, Generic
-from _wrap import merge, wrapper
+from _basic import OneSide, OneSide2, Strategy, Generic
+from _wrap import merge, wrapper, wrapper2
 from marketsim import order, orderbook, scheduler, mathutils, types, registry, bind, meta
 from marketsim.types import *
+
+class _LiquidityProviderSide2_Impl(OneSide2):
+    
+    @property
+    def _orderFactory(self):
+        return self.orderFactoryT(self.side)
+    
+    @property
+    def _eventGen(self):
+        return scheduler.Timer(self.creationIntervalDistr, self._scheduler)
+        
+    def _orderFunc(self):
+        queue = self._trader.book.queue(self.side)
+        currentPrice = queue.best.price if not queue.empty else\
+                       queue.lastPrice if queue.lastPrice is not None else\
+                       self.defaultValue
+        price = currentPrice * self.priceDistr()
+        volume = int(self.volumeDistr())
+        return (price, volume)
+    
+    def dispose(self):
+        OneSide2.dispose(self)
+        self._eventGen.cancel()
+
+exec wrapper2("LiquidityProviderSide2",
+             """ Liquidity provider for one side has followng parameters:
+
+                 |side|
+                     side of orders to create (default: Side.Sell)
+                     
+                 |orderFactory| 
+                     order factory function (default: order.Limit.T)
+                     
+                 |initialValue| 
+                     initial price which is taken if orderBook is empty (default: 100)
+                     
+                 |creationIntervalDistr|
+                     defines intervals of time between order creation 
+                     (default: exponential distribution with |lambda| = 1)
+                     
+                 |priceDistr|
+                     defines multipliers for current asset price when price of
+                     order to create is calculated (default: log normal distribution with 
+                     |mu| = 0 and |sigma| = 0.1)
+                     
+                 |volumeDistr| 
+                     defines volumes of orders to create 
+                     (default: exponential distribution with |lambda| = 1)
+
+                 It wakes up in moments of time given by *creationIntervalDistr*, checks
+                 the last best price of orders in the corresponding queue, takes *initialValue*
+                 if it is empty, multiplies it by a value taken from *priceDistr* to obtain price
+                 of the order to create, calculates order volume using *volumeDistr*, creates
+                 an order via *orderFactoryT(side)* and tells the trader to send it.
+             """,
+             [('side',                  'Side.Sell',                            'Side'),
+              ('orderFactoryT',         'order.LimitFactory',                   'Side -> (Price, Volume) -> IOrder'),
+              ('defaultValue',          '100',                                  'Price'),
+              ('creationIntervalDistr', 'mathutils.rnd.expovariate(1.)',        '() -> TimeInterval'),
+              ('priceDistr',            'mathutils.rnd.lognormvariate(0., .1)', '() -> float'),
+              ('volumeDistr',           'mathutils.rnd.expovariate(1.)',        '() -> Volume')])
+
 
 class _LiquidityProviderSide_Impl(OneSide):
 
@@ -125,6 +187,75 @@ def LiquidityProviderSideEx(side                    = Side.Sell,
     r._alias = ["Generic", 'LiquidityProviderSide']
     
     return r
+
+def merge_dict(d, **kwargs):
+    ret = d.copy()
+    for k in kwargs:
+        ret[k] = kwargs[k]
+    return ret
+
+
+class _LiquidityProvider2_Impl(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, None)
+        props = { k : getattr(self, k) for k in self._properties.iterkeys() }
+        sp = merge_dict(props, side=Side.Sell)
+        bp = merge_dict(props, side=Side.Buy) 
+        self._sell = LiquidityProviderSide2(**props)
+        self._buy = LiquidityProviderSide2(**props)
+        
+    _properties = { '_sell' : types.IStrategy,
+                    '_buy'  : types.IStrategy }
+        
+    def reset(self):
+        self._sell.reset()
+        self._buy.reset()
+    
+    def suspend(self, s):
+        Strategy.suspend(self, s)
+        self._sell.suspend(s)
+        self._buy.suspend(s)
+        
+    @property
+    def suspended(self):
+        assert self._sell.suspended == self._suspended
+        assert self._buy.suspended == self._suspended
+        return Strategy.suspended(self)
+    
+    def dispose(self):
+        self._sell.dispose()
+        self._buy.dispose()
+
+exec wrapper2('LiquidityProvider2',
+             """ Liquidity provider is a combination of two LiquidityProviderSide traders 
+                 with the same parameters but different trading sides. 
+                 
+                 It has followng parameters:
+
+                 |orderFactory| 
+                     order factory function (default: order.Limit.T)
+                     
+                 |initialValue| 
+                     initial price which is taken if orderBook is empty (default: 100)
+                     
+                 |creationIntervalDistr|
+                     defines intervals of time between order creation 
+                     (default: exponential distribution with |lambda| = 1)
+                     
+                 |priceDistr|
+                     defines multipliers for current asset price when price of
+                     order to create is calculated (default: log normal distribution with 
+                     |mu| = 0 and |sigma| = 0.1)
+                     
+                 |volumeDistr| 
+                     defines volumes of orders to create 
+                     (default: exponential distribution with |lambda| = 1)
+            """,  
+            [('orderFactoryT',          'order.LimitFactory',                   'Side -> (Price, Volume) -> IOrder'),
+             ('defaultValue',           '100',                                  'Price'),
+             ('creationIntervalDistr',  'mathutils.rnd.expovariate(1.)',        '() -> TimeInterval'),
+             ('priceDistr',             'mathutils.rnd.lognormvariate(0., .1)', '() -> float'),
+             ('volumeDistr',            'mathutils.rnd.expovariate(.1)',        '() -> Volume')])
 
 class _LiquidityProvider_Impl(Strategy):
     def __init__(self, trader, params):
