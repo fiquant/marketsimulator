949f4771d3f8c25ab6f525ca7411e3684e3fd0f0
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-05-22 15:28:05 +0000

    strategy/_mean_reversion.py added
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@584 c58600cb-c4ea-42d0-b18c-69d24e00f01c

---
+++ marketsim/strategy/_mean_reversion.py
@@ -0,0 +1,77 @@
+from marketsim import (scheduler, observable, types, meta, 
+                       Side, registry, orderbook, bind, order, mathutils)
+
+from _generic import Generic
+from _signal import SignalBase, SignalValue, SignalEvent
+from _wrap import wrapper2
+from _fv import FundamentalValueBase, FundamentalValueSide
+
+from marketsim.types import *
+
+
+class _MeanReversion_Impl(FundamentalValueBase):
+    
+    def __init__(self):
+        self._eventGen = scheduler.Timer(self.creationIntervalDistr)
+        myBook = orderbook.OfTrader()
+        self._fundamentalValue = observable.Fold(observable.Price(myBook), 
+                                                 self.average)
+        FundamentalValueBase.__init__(self)
+
+    @property
+    def _orderFactoryT(self):
+        return self.orderFactory
+
+    _internals = ['_eventGen', '_fundamentalValue']
+    
+    @property
+    def _volume(self): 
+        return bind.Method(self, 'volumeDistr')  
+
+exec wrapper2("MeanReversion",
+             """ Mean reversion strategy believes that asset price should return to its average value.
+                 It estimates this average using some functional and 
+                 if the current asset price is lower than the average
+                 it buys the asset and if the price is higher it sells the asset. 
+             
+                 It has following parameters: 
+                 
+                 |orderFactory| 
+                     order factory function (default: order.Market.T)
+                 
+                 |creationIntervalDistr| 
+                     defines intervals of time between order creation 
+                     (default: exponential distribution with |lambda| = 1)
+                 
+                 |average| 
+                     functional used to calculate the average 
+                     (default: exponentially weighted moving average with |alpha| = 0.15)
+                     
+                 |volumeDistr| 
+                     defines volumes of orders to create 
+                     (default: exponential distribution with |lambda| = 1)
+             """,
+             [('orderFactory',          'order.MarketFactory',              'Side -> Volume -> IOrder'),
+              ('average',               'mathutils.ewma(alpha = 0.15)',     'IUpdatableValue'),
+              ('volumeDistr',           'mathutils.rnd.expovariate(1.)',    '() -> Volume'),
+              ('creationIntervalDistr', 'mathutils.rnd.expovariate(1.)',    '() -> TimeInterval')])
+
+
+
+def MeanReversionEx   (average               = mathutils.ewma(alpha = 0.15),
+                       orderFactory          = order.MarketFactory, 
+                       volumeDistr           = mathutils.rnd.expovariate(1.), 
+                       creationIntervalDistr = mathutils.rnd.expovariate(1.)):
+
+    orderBook = orderbook.OfTrader()
+    avg = observable.Fold(observable.Price(orderBook), average)
+    
+    r = Generic(orderFactory= orderFactory, 
+                volumeFunc  = volumeDistr, 
+                eventGen    = scheduler.Timer(creationIntervalDistr), 
+                sideFunc    = FundamentalValueSide(orderBook, avg))
+    
+    r._alias = ["Generic", "MeanReversion"]
+    
+    return r
+
