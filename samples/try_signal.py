cc164d1ecbba6d4e55ee4eaf9066e519f1bb14ac
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-06-07 16:45:40 +0000

    talk part about web interface
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@636 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- samples/try_signal.py
+++ samples/try_signal.py
@@ -1,31 +1,23 @@
 import sys
 sys.path.append(r'..')
 
-from marketsim import (signal, strategy, trader, orderbook, 
-                       timeserie, scheduler, observable, veusz, mathutils)
-
+from marketsim import (signal, strategy, observable, mathutils)
 from common import run
-
-const = mathutils.constant
 
 def Signal(ctx):
 
-    ctx.volumeStep = 30
-
+    const = mathutils.constant
     linear_signal = signal.RandomWalk(initialValue=20, 
                                       deltaDistr=const(-.1), 
                                       label="20-0.1t")
 
     return [
-        ctx.makeTrader_A(strategy.LiquidityProvider(volumeDistr=const(2)),
-                         "liquidity"),
+        ctx.makeTrader_A(strategy.LiquidityProvider(volumeDistr=const(4)), "liquidity"),
         
-        ctx.makeTrader_A(strategy.Signal(linear_signal), 
-                         "signal", 
+        ctx.makeTrader_A(strategy.Signal(linear_signal), "signal", 
                          [(linear_signal, ctx.amount_graph)]),
     
-        ctx.makeTrader_A(strategy.SignalEx(linear_signal), 
-                         "signal_ex")
+        ctx.makeTrader_A(strategy.SignalEx(linear_signal), "signal_ex")
     ]    
 
 if __name__ == '__main__':
