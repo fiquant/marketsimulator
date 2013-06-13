46b255a2259ec2df5131ccecb9048260149c3ba9
antonkolotaev <antonkolotaev@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-06-13 14:47:18 +0000

    splitting observable/_computed.py
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@658 c58600cb-c4ea-42d0-b18c-69d24e00f01c

--- marketsim/observable/__init__.py
+++ marketsim/observable/__init__.py
@@ -1,8 +1,12 @@
-from _computed import InstEfficiency, PnL, Price, CrossSpread, PriceAtVolume, VolumeLevels,\
-                        VolumeTraded, BestPrice, BidPrice, AskPrice, OnEveryDt
+from _computed import OnEveryDt
                         
 from _average import EWMA, dEWMA, derivative, Fold, avg, trend
 
 from _async import Efficiency
 
 from _momentum import RSI
+
+from _orderbook import (Price, CrossSpread, PriceAtVolume, VolumeLevels, 
+                        BestPrice, BidPrice, AskPrice)
+
+from _trader import  InstEfficiency, PnL, VolumeTraded
---
+++ marketsim/observable/_orderbook.py
@@ -0,0 +1,211 @@
+from marketsim import meta, Event, types, Side, event, scheduler, mathutils, getLabel
+
+from _computed import IndicatorBase
+
+### ------------------------------------------------  data accessors
+
+class mid_price(object):
+    """ Returns middle price in the given *orderbook*
+    """
+    
+    def __init__(self, orderbook):
+        self.orderbook = orderbook
+        self._alias = ["Asset's", "Mid-price"]
+        
+    _types = [meta.function((), float)]
+    
+    def __call__(self):
+        return self.orderbook.price
+
+    @property
+    def digits(self):
+        return self.orderbook._digitsToShow
+    
+    @property
+    def label(self):
+        return "Price_{" + getLabel(self.orderbook) + "}"
+    
+    _properties = { 'orderbook' : types.IOrderBook }
+
+class cross_spread(object):
+    
+    def __init__(self, book_A, book_B):
+        self.book_A = book_A
+        self.book_B = book_B
+        
+    def __call__(self):
+        asks = self.book_A.asks
+        bids = self.book_B.bids
+        return asks.best.price - bids.best.price if not asks.empty and not bids.empty else None
+    
+    @property
+    def label(self):
+        return "Price("+self.book_A.asks.label+") - Price("+self.book_B.bids.label+")"
+
+class side_price(object):
+    """ Returns *orderbook* *side* price 
+    """
+    
+    def __init__(self, orderbook, side):
+        self.orderbook = orderbook
+        self.side = side
+        self._alias = ["Asset's", "Side price"]
+        
+    _types = [meta.function((), float)]
+    
+    def __call__(self):
+        queue = self.orderbook.queue(self.side)
+        return queue.best.price if not queue.empty else None
+    
+    @property
+    def digits(self):
+        return self.orderbook._digitsToShow
+    
+    @property
+    def label(self):
+        return "Price("+self.orderbook.queue(self.side).label+")" 
+    
+    _properties = { 'orderbook' : types.IOrderBook, 
+                    'side'      : types.Side }
+    
+class price_at_volume(object):
+
+    def __init__(self, orderbook, side, volumeAt):
+        self.orderbook = orderbook
+        self.side = side
+        self.volumeAt = volumeAt
+        self._alias = ["Asset's", "Price at Volume"]
+        
+    _types = [meta.function((), float)]
+    
+    def __call__(self):
+        queue = self.orderbook.queue(self.side)
+        for (volume, price) in queue.getVolumePrices([self.volumeAt]):
+            return price
+        return None
+    
+    @property
+    def digits(self):
+        return self.orderbook._digitsToShow
+    
+    @property
+    def label(self):
+        return "PriceAtVolume_{"+str(self.volumeAt)+"}("+self.orderbook.queue(self.side).label+")" 
+    
+    _properties = { 'orderbook' : types.IOrderBook, 
+                    'side'      : types.Side, 
+                    'volumeAt'  : float }
+
+class volume_levels(object):
+    
+    def __init__(self, orderbook, side, volumeDelta, volumeCount):
+        self.orderbook = orderbook
+        self.side = side
+        self.volumeDelta = volumeDelta
+        self.volumeCount = volumeCount
+        self._alias = ["Asset's", "Volume levels"]
+    
+    @property    
+    def volumes(self):
+        return [self.volumeDelta * i for i in range(self.volumeCount)]
+        
+    _types = [meta.function((), float)] # should be () -> meta.listOf(float)
+    
+    def __call__(self):
+        queue = self.orderbook.queue(self.side)
+        return [price for (volume, price) in queue.getVolumePrices(self.volumes)]
+    
+    @property
+    def digits(self):
+        return self.orderbook._digitsToShow
+    
+    @property
+    def label(self):
+        return "VolumeLevels("+self.orderbook.queue(self.side).label+")" 
+    
+    _properties = { 'orderbook'     : types.IOrderBook, 
+                    'side'          : types.Side, 
+                    'volumeDelta'   : float,
+                    'volumeCount'   : int  }
+
+### -------------------------------------------------------------------   Events
+    
+class OnPriceChanged(Event):
+    """ Event that is fired once mid-price in the *orderbook* has changed
+    """
+    
+    def __init__(self, orderbook):
+        Event.__init__(self)
+        self.orderbook = orderbook
+        event.subscribe(self.orderbook.on_price_changed, self.fire, self)
+        
+    _properties = { 'orderbook' : types.IOrderBook }
+
+class OnSideBestChanged(Event):
+    """ Event that is fired once a *side* price in the *orderbook* has changed
+    """
+    
+    def __init__(self, orderbook, side):
+        Event.__init__(self)
+        self.orderbook = orderbook
+        self.side = side
+        
+    def bind(self, context): # TODO: we should subscribe to orderbook and side changed events
+        self.orderbook.queue(self.side).on_best_changed += self.fire
+        
+    _properties = { 'orderbook' : types.IOrderBook, 
+                    'side'      : types.Side }
+    
+ 
+### -------------------------------------------------------------------   Observables
+    
+def Price(book):
+    """ Creates an indicator bound to the middle price of an asset
+    """   
+    return IndicatorBase(OnPriceChanged(book), mid_price(book))
+    
+   
+def CrossSpread(book_A, book_B):
+    """ Returns indicator bound to difference between ask of book_A and bid of book_B
+    """
+    asks = book_A.asks
+    bids = book_B.bids
+    asks_changed = OnSideBestChanged(book_A, Side.Sell)
+    bids_changed = OnSideBestChanged(book_B, Side.Buy)
+    return IndicatorBase(\
+         event.Array([asks_changed, bids_changed]), 
+         cross_spread(book_A, book_B))
+
+def BestPrice(book, side, label):
+    """ Creates an indicator bound to the price of the best order in a queue
+    book - asset order book
+    side - side of the queue
+    label - label prefix
+    """
+    
+    return IndicatorBase(OnSideBestChanged(book, side), side_price(book, side))
+    
+def BidPrice(book):
+    """ Creates an indicator bound to the bid price of the asset
+    book - asset order book
+    """
+    return BestPrice(book, Side.Buy, "BidPrice ")
+
+def AskPrice(book):
+    """ Creates an indicator bound to the ask price of the asset
+    book - asset order book
+    """
+    return BestPrice(book, Side.Sell, "AskPrice ")
+
+def PriceAtVolume(interval, orderbook, side, volume):
+
+    return IndicatorBase(scheduler.Timer(mathutils.constant(interval)),
+                         price_at_volume(orderbook, side, volume), 
+                         {'smooth':True, 'fillBelow' : side == Side.Buy, 'fillAbove' : side == Side.Sell})
+
+def VolumeLevels(interval, orderbook, side, volumeDelta, volumeCount):
+
+    return IndicatorBase(scheduler.Timer(mathutils.constant(interval)),
+                         volume_levels(orderbook, side, volumeDelta, volumeCount), 
+                         {'smooth':True, 'volumeLevels' : True, 
+                          'fillBelow' : side == Side.Buy, 'fillAbove' : side == Side.Sell})
