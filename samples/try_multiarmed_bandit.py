66aeba03ec3e520014954cb3ef3b33709064e372
kpodkanski <kpodkanski@c58600cb-c4ea-42d0-b18c-69d24e00f01c>
2013-06-13 12:45:50 +0000

    MultiarmedBandit strategy
    git-svn-id: http://svn.code.sf.net/p/marketsimulator/svn/DevAnton/v3@651 c58600cb-c4ea-42d0-b18c-69d24e00f01c

---
+++ samples/try_multiarmed_bandit.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append(r'..')
+
+from marketsim import (strategy, orderbook, trader, order, 
+                       timeserie, scheduler, observable, veusz, mathutils)
+
+from common import run
+
+const = mathutils.constant
+
+def MultiarmedBandit(ctx):
+    
+    ctx.volumeStep = 100
+
+    c_200 = const(200.)
+    
+    fv_200_12 = strategy.FundamentalValue(fundamentalValue=c_200, volumeDistr=const(12))
+
+    fv_200 = fv_200_12.With(volumeDistr=const(1.), creationIntervalDistr=const(1.))
+     
+    def s_fv(fv):
+        return fv_200.With(fundamentalValue=const(fv))
+
+    def fv_virtual(fv):
+        return ctx.makeTrader_A(s_fv(fv), "v" + str(fv))
+
+    return [
+            ctx.makeTrader_A( 
+                      strategy.LiquidityProvider(
+                            volumeDistr=const(70.), 
+                            orderFactoryT=order.WithExpiryFactory(
+                                expirationDistr=const(10))), 
+                      "liquidity"), 
+            
+    
+            ctx.makeTrader_A(fv_200_12, "t200"),    
+    
+            fv_virtual(100.),
+            fv_virtual(150.),
+            fv_virtual(200.),
+            fv_virtual(250.),
+            fv_virtual(300.),
+            
+            ctx.makeTrader_A(strategy.chooseTheBest([
+                                   s_fv(100.),
+                                   s_fv(150.), 
+                                   s_fv(200.), 
+                                   s_fv(250.), 
+                                   s_fv(300.), 
+                                   ]),
+                 "best"),
+    
+            ctx.makeTrader_A(strategy.multiarmedBandit([
+                                               s_fv(100.),
+                                               s_fv(150.), 
+                                               s_fv(200.), 
+                                               s_fv(250.), 
+                                               s_fv(300.), 
+                                               ]),
+                             "bandit")
+    ]     
+
+if __name__ == '__main__':
+    run('multiarmed_bandit', MultiarmedBandit)
