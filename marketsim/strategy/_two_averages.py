7313e5c44b45a89e3840340e6f997db14a39190c
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-03 11:11:49 +0200

    TwoAverages uses new EWMA

--- marketsim/strategy/_two_averages.py
+++ marketsim/strategy/_two_averages.py
@@ -12,8 +12,8 @@ class _TwoAverages_Impl(SignalBase):
     def __init__(self):
         self._eventGen = scheduler.Timer(self.creationIntervalDistr)
         price = observable.Price(orderbook.OfTrader())
-        self._average1 = observable.Fold(price, self.average1)
-        self._average2 = observable.Fold(price, self.average2)
+        self._average1 = observable.EWMA(price, self.ewma_alpha1)
+        self._average2 = observable.EWMA(price, self.ewma_alpha2)
         SignalBase.__init__(self)
         
     _internals = ['_average1', '_average2']
@@ -35,13 +35,14 @@ exec wrapper2("TwoAverages",
                       functional used to obtain the first average
                       (defaut: expenentially weighted moving average with |alpha| = 0.15)
                       
-                 |average2| 
-                      functional used to obtain the second average
-                      (defaut: expenentially weighted moving average with |alpha| = 0.015)
+                 |ewma_alpha1| 
+                     parameter |alpha| for the first exponentially weighted moving average
+                     (default: 0.15.)
                       
-                 |orderFactory| 
-                     order factory function (default: order.Market.T)
-                     
+                 |ewma_alpha2| 
+                     parameter |alpha| for the second exponentially weighted moving average
+                     (default: 0.015.)
+                      
                  |threshold| 
                      threshold when the trader starts to act (default: 0.)
                      
@@ -53,8 +54,8 @@ exec wrapper2("TwoAverages",
                      defines intervals of time between order creation 
                      (default: exponential distribution with |lambda| = 1)                     
              """,
-             [('average1',              'mathutils.ewma(alpha = 0.15)',  'IUpdatableValue'),
-              ('average2',              'mathutils.ewma(alpha = 0.015)', 'IUpdatableValue'),
+             [('ewma_alpha1',           'non_negative',                  '0.15'),
+              ('ewma_alpha2',           'non_negative',                  '0.015'),
               ('threshold',             '0.',                            'non_negative'), 
               ('orderFactory',          'order.MarketFactory',           'Side -> Volume -> IOrder'),
               ('creationIntervalDistr', 'mathutils.rnd.expovariate(1.)', '() -> TimeInterval'),
@@ -62,22 +63,19 @@ exec wrapper2("TwoAverages",
 
   
 @registry.expose(["Periodic", 'TwoAverages'], args = ())
-def TwoAveragesEx(average1 = None, #mathutils.ewma(alpha = 0.15), 
-                  average2 = None, #mathutils.ewma(alpha = 0.015), 
+def TwoAveragesEx(ewma_alpha1           = 0.15, 
+                  ewma_alpha2           = 0.015, 
                   threshold             = 0, 
                   orderFactory          = order.MarketFactory, 
                   creationIntervalDistr = mathutils.rnd.expovariate(1.), 
                   volumeDistr           = mathutils.rnd.expovariate(1.)):
-    
-    if average1 is None: average1 = mathutils.ewma(alpha = 0.15)
-    if average2 is None: average2 = mathutils.ewma(alpha = 0.015)
     
     return defs(
         Periodic(orderFactory= orderFactory, 
                  volumeFunc  = volumeDistr,
                  eventGen    = scheduler.Timer(creationIntervalDistr),
                  sideFunc    = SignalSide(
-                                  (observable.Fold(_.price, average1) 
-                                   - observable.Fold(_.price, average2)),
+                                  (observable.EWMA(_.price, ewma_alpha1) 
+                                   - observable.EWMA(_.price, ewma_alpha2)),
                                   threshold)), 
         { 'price' : observable.Price(orderbook.OfTrader()) })
