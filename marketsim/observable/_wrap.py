feef6f9081fcce9badbe8ff64ccd7e435fdc2bb9
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-20 14:03:36 +0200

    common wrapper implementation moved to wrap.py

--- marketsim/observable/_wrap.py
+++ marketsim/observable/_wrap.py
@@ -1,84 +1,22 @@
 import marketsim
-from marketsim import ops, types, event, _, flags
+from marketsim import wrap, ops, types, event, _, flags
 
-class Base(object):
-
-    def __init__(self):
-        self.impl = self.getImpl()
-        if 'getDefinitions' in dir(self):
-            self.impl._definitions = self.getDefinitions()
-                
-    def __call__(self):
-        return self.impl()
-    
-    @property
-    def label(self):
-        return self.impl.label
-
-FunctionBase = types.Factory('FunctionBase', """(Base):
+FunctionBase = types.Factory('FunctionBase', """(wrap.Base):
     _properties = {'impl' : (types.IFunction[%(T)s], flags.collapsed) }
 """, globals())
 
-ObservableBase = types.Factory('ObservableBase', """(Base):
+ObservableBase = types.Factory('ObservableBase', """(wrap.Base):
 
     def __init__(self):
-        Base.__init__(self)
+        wrap.Base.__init__(self)
         event.subscribe(self.impl, _(self).fire, self)
         
     _properties = {'impl' : (types.IObservable[%(T)s], flags.collapsed) }
 """, globals())
         
-tmpl = """
-%(reg)s
-class %(name)s_Generated(%(name)s, _wrap.%(kind)sBase[%(name)s.T]):
-    \"\"\" %(docstring)s
-    \"\"\"    
-    
-    def __init__(self, %(args)s):
-        %(ctor)s
-        _wrap.%(kind)sBase[%(name)s.T].__init__(self)
-        %(name)s_Impl.__init__(self)
-        
-    @property
-    def attributes(self):
-        return {}
-
-    _properties = { %(props)s }
-"""
-
-prop = """
-    @property
-    def %(name)s(self):
-        return self._%(name)s
-        
-    @%(name)s.setter
-    def %(name)s(self, value):
-        self._%(name)s = value
-        self.reset()
-        
-"""
-
-trailer = """    
-%(name)s_Impl = %(name)s
-%(name)s = %(name)s_Generated
-"""
-
-def _generate(kind, cls, alias, docstring, fields, ctx):
-    def process(tmpl, sep=", "):
-        return sep.join([tmpl % locals() for (name, ini, typ) in fields])
-    
-    args = process("%(name)s = None")
-    ctor = process("self._%(name)s = %(name)s if %(name)s is not None else %(ini)s", "; ")
-    props= process("\'%(name)s\' : %(typ)s")
-    binds = process("ctx.%(name)s = self._%(name)s", "; ")
-    pdefs = "".join([prop % locals() for (name, _,_) in fields])
-    reg = "@registry.expose("+str(alias)+")"# if register else ""
-    name = cls.__name__
-    #print (tmpl + pdefs + trailer) % locals()
-    exec (tmpl + pdefs + trailer) % locals() in ctx
  
 def function(cls, alias, docstring, fields, ctx):
-    _generate("Function", cls, alias, docstring, fields, ctx)
+    wrap.generate("Function", cls, alias, docstring, fields, ctx)
     
 def observable(cls, alias, docstring, fields, ctx):
-    _generate("Observable", cls, alias, docstring, fields, ctx)
+    wrap.generate("Observable", cls, alias, docstring, fields, ctx)
---
+++ marketsim/wrap.py
@@ -0,0 +1,62 @@
+class Base(object):
+
+    def __init__(self):
+        self.impl = self.getImpl()
+        if 'getDefinitions' in dir(self):
+            self.impl._definitions = self.getDefinitions()
+                
+    def __call__(self):
+        return self.impl()
+    
+    @property
+    def label(self):
+        return self.impl.label
+
+tmpl = """
+%(reg)s
+class %(name)s_Generated(%(name)s, _wrap.%(kind)sBase[%(name)s.T]):
+    \"\"\" %(docstring)s
+    \"\"\"    
+    
+    def __init__(self, %(args)s):
+        %(ctor)s
+        _wrap.%(kind)sBase[%(name)s.T].__init__(self)
+        %(name)s_Impl.__init__(self)
+        
+    @property
+    def attributes(self):
+        return {}
+
+    _properties = { %(props)s }
+"""
+
+prop = """
+    @property
+    def %(name)s(self):
+        return self._%(name)s
+        
+    @%(name)s.setter
+    def %(name)s(self, value):
+        self._%(name)s = value
+        self.reset()
+        
+"""
+
+trailer = """    
+%(name)s_Impl = %(name)s
+%(name)s = %(name)s_Generated
+"""
+
+def generate(kind, cls, alias, docstring, fields, ctx):
+    def process(tmpl, sep=", "):
+        return sep.join([tmpl % locals() for (name, ini, typ) in fields])
+    
+    args = process("%(name)s = None")
+    ctor = process("self._%(name)s = %(name)s if %(name)s is not None else %(ini)s", "; ")
+    props= process("\'%(name)s\' : %(typ)s")
+    binds = process("ctx.%(name)s = self._%(name)s", "; ")
+    pdefs = "".join([prop % locals() for (name, _,_) in fields])
+    reg = "@registry.expose("+str(alias)+")"# if register else ""
+    name = cls.__name__
+    #print (tmpl + pdefs + trailer) % locals()
+    exec (tmpl + pdefs + trailer) % locals() in ctx
