cc4b0f2cfbe36b41e3ec9c9fbc7b4a236869a6c8
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-20 17:51:09 +0200

    strategy.RSI*** are auto generated

--- marketsim/strategy/_rsi.py
+++ marketsim/strategy/_rsi.py
@@ -49,34 +49,83 @@ def RSIEx    (         alpha                 = 1./14,
     
     return r
 
-@registry.expose(["Periodic", "RSIbis"], args=())
-def RSIbis (timeframe               = 1., 
-            threshold               = 30,
-            alpha                   = 1./14,
-            orderFactory            = order.MarketFactory, 
-            volumeDistr             = mathutils.rnd.expovariate(1.), 
-            creationIntervalDistr   = mathutils.rnd.expovariate(1.)):
+import _wrap
+
+class RSIbis(types.ISingleAssetStrategy):
+
+    def getDefinitions(self):
+        return { 'rsi' : observable.RSI(orderbook.OfTrader(), 
+                                        self.timeframe, 
+                                        self.alpha) }
     
-    thisBook = orderbook.OfTrader()
+    def getImpl(self):
+        return Periodic(orderFactory = self.orderFactory, 
+                        volumeFunc   = self.volumeDistr, 
+                        eventGen     = scheduler.Timer(self.creationIntervalDistr),
+                        sideFunc     = observable.side.Signal(
+                                                ops.constant(50) - _.rsi, 
+                                                50-self.threshold))
+        
+_wrap.strategy(RSIbis, ['Periodic', 'RSI bis'], 
+               """
+               """, 
+               [
+                  ('alpha',                 '1./14',                         'non_negative'), 
+                  ('timeframe',             '1.',                            'non_negative'), 
+                  ('threshold',             '30.',                           'non_negative'), 
+                  ('orderFactory',          'order.MarketFactory',           'Side -> Volume -> IOrder'),
+                  ('creationIntervalDistr', 'mathutils.rnd.expovariate(1.)', '() -> TimeInterval'),
+                  ('volumeDistr',           'mathutils.rnd.expovariate(1.)', '() -> Volume')
+               ], globals())
+
+class RSIEx(types.ISingleAssetStrategy):
+
+    def getDefinitions(self):
+        return { 'rsi' : observable.Fold(observable.MidPrice(orderbook.OfTrader()),
+                                         mathutils.rsi(self.alpha)) }
     
-    return defs(Periodic(orderFactory = orderFactory, 
-                         volumeFunc   = volumeDistr, 
-                         eventGen     = scheduler.Timer(creationIntervalDistr),
-                         sideFunc     = observable.side.Signal(ops.constant(50) - _.rsi, 
-                                                               50-threshold)), 
-                { 'rsi' : observable.RSI(thisBook, timeframe, alpha) })
+    def getImpl(self):
+        return  Periodic(orderFactory= self.orderFactory, 
+                         volumeFunc  = self.volumeDistr, 
+                         eventGen    = scheduler.Timer(self.creationIntervalDistr), 
+                         sideFunc    = RelativeStrengthIndexSide(orderbook.OfTrader(), 
+                                                                 _.rsi, 
+                                                                 self.threshold))
+        
+_wrap.strategy(RSIEx, ['Periodic', 'RSI ex'], 
+               """
+               """, 
+               [
+                  ('alpha',                 '1./14',                         'non_negative'), 
+                  ('threshold',             '30.',                           'non_negative'), 
+                  ('orderFactory',          'order.MarketFactory',           'Side -> Volume -> IOrder'),
+                  ('creationIntervalDistr', 'mathutils.rnd.expovariate(1.)', '() -> TimeInterval'),
+                  ('volumeDistr',           'mathutils.rnd.expovariate(1.)', '() -> Volume')
+               ], globals())
+
     
 from _desired_position import DesiredPosition
+
+class RSI_linear(types.ISingleAssetStrategy):
+
+    def getDefinitions(self):
+        return { 'rsi' : observable.RSI(orderbook.OfTrader(), 
+                                        self.timeframe, 
+                                        self.alpha) }
     
-@registry.expose(['Desired position', 'RSI linear'])
-def RSI_linear(timeframe               = 1., 
-               alpha                   = 1./14,
-               k                       = ops.constant(-0.04),
-               orderFactory            = order.MarketFactory):
+    def getImpl(self):
+        return DesiredPosition(
+                    orderFactory = self.orderFactory, 
+                    desiredPosition = observable.OnEveryDt(1, 
+                                            (ops.constant(50) - _.rsi) * self.k))
+
+_wrap.strategy(RSI_linear, ['Desired position', 'RSI linear'], 
+               """
+               """, 
+               [
+                  ('alpha',                 '1./14',                         'non_negative'), 
+                  ('k',                     'ops.constant(-0.04)',           'IFunction[float]'), 
+                  ('timeframe',             '1.',                            'non_negative'), 
+                  ('orderFactory',          'order.MarketFactory',           'Side -> Volume -> IOrder'),
+               ], globals())
     
-    thisBook = orderbook.OfTrader()
-    
-    return defs(DesiredPosition(
-                        orderFactory = orderFactory, 
-                        desiredPosition = observable.OnEveryDt(1, (ops.constant(50) - _.rsi) * k)), 
-                { 'rsi' : observable.RSI(thisBook, timeframe, alpha) })
