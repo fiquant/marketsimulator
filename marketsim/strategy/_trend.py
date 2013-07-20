fe9bb750b75582d074bf0e86c158b48ff623c7fe
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-20 17:17:06 +0200

    strategy.TrendFollowerEx is auto generated

--- marketsim/strategy/_trend.py
+++ marketsim/strategy/_trend.py
@@ -1,6 +1,6 @@
 from marketsim.types import *
 from marketsim import (orderbook, observable, scheduler, order, mathutils, types, meta, 
-                       registry, signal, bind, ops)
+                       registry, signal, bind, ops, _)
 from _periodic import Periodic
 from _signal import SignalBase
 
@@ -55,21 +55,59 @@ exec wrapper2('TrendFollower',
               ('creationIntervalDistr',  'mathutils.rnd.expovariate(1.)', '() -> TimeInterval'),
               ('volumeDistr',            'mathutils.rnd.expovariate(1.)', '() -> Volume')])
 
-@registry.expose(["Periodic", 'TrendFollower'], args = ())
-def TrendFollowerEx(ewma_alpha              = 0.15, 
-                    threshold               = 0., 
-                    orderFactory            = order.MarketFactory, 
-                    creationIntervalDistr   = mathutils.rnd.expovariate(1.), 
-                    volumeDistr             = mathutils.rnd.expovariate(1.)):
+import _wrap
+
+class TrendFollowerEx(types.ISingleAssetStrategy):
     
-    orderBook = orderbook.OfTrader()
-    trend = ops.Derivative(
-                observable.EWMA(
-                    observable.MidPrice(orderBook),
-                    ewma_alpha))
+    def getDefinitions(self):
+        orderBook = orderbook.OfTrader()
+        
+        return { 
+                 'trend' : ops.Derivative(
+                                observable.EWMA(
+                                    observable.MidPrice(orderBook),
+                                    self.ewma_alpha))
+            }
     
-    return Periodic(orderFactory= orderFactory, 
-                    volumeFunc  = volumeDistr,
-                    eventGen    = scheduler.Timer(creationIntervalDistr),
-                    sideFunc    = observable.side.Signal(trend, threshold))
-    
+    def getImpl(self):
+        return Periodic(orderFactory= self.orderFactory, 
+                        volumeFunc  = self.volumeDistr,
+                        eventGen    = scheduler.Timer(self.creationIntervalDistr),
+                        sideFunc    = observable.side.Signal(_.trend, self.threshold))
+
+_wrap.strategy(TrendFollowerEx, ['Periodic', 'TrendFollower'], 
+                 """ Trend follower can be considered as a sort of a signal strategy 
+                     where the *signal* is a trend of the asset. 
+                     Under trend we understand the first derivative of some moving average of asset prices. 
+                     If the derivative is positive, the trader buys; if negative - it sells.
+                     Since moving average is a continuously changing signal, we check its
+                     derivative at random moments of time given by *creationIntervalDistr*. 
+                     
+                     It has following parameters:
+                    
+                     |ewma_alpha| 
+                         parameter |alpha| for exponentially weighted moving average
+                         (default: 0.15.)
+                         
+                     |orderFactory| 
+                         order factory function (default: order.Market.T)
+                         
+                     |threshold| 
+                         threshold when the trader starts to act (default: 0.)
+                         
+                     |creationIntervalDistr|
+                         defines intervals of time between order creation
+                         (default: exponential distribution with |lambda| = 1)
+                         
+                     |volumeDistr| 
+                         defines volumes of orders to create 
+                         (default: exponential distribution with |lambda| = 1)
+                 """,
+                 [
+                  ('ewma_alpha',             '0.15',                          'non_negative'),
+                  ('threshold',              '0.',                            'non_negative'), 
+                  ('orderFactory',           'order.MarketFactory',           'Side -> Volume -> IOrder'),
+                  ('creationIntervalDistr',  'mathutils.rnd.expovariate(1.)', '() -> TimeInterval'),
+                  ('volumeDistr',            'mathutils.rnd.expovariate(1.)', '() -> Volume')
+                 ], globals())
+
