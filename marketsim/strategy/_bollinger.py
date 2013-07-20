b1efa320bf6fea65d7e9ff8c031c4494a479a39a
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-20 18:00:25 +0200

    Bollinger_linear is auto generated

--- marketsim/strategy/_bollinger.py
+++ marketsim/strategy/_bollinger.py
@@ -1,18 +1,27 @@
-from marketsim import _, defs, ops, order, orderbook, observable, registry
+from marketsim import _, defs, ops, order, orderbook, observable, registry, types
+from marketsim.types import *
 
 from _desired_position import DesiredPosition
-    
-@registry.expose(['Desired position', 'Bollinger linear'])
-def Bollinger_linear(alpha                   = 0.15,
-                     k                       = ops.constant(0.5),
-                     orderFactory            = order.MarketFactory):
-    
-    thisBook = orderbook.OfTrader()
-    
-    return defs(DesiredPosition(
-                        orderFactory = orderFactory, 
-                        desiredPosition = observable.IndicatorBase(_.price, 
-                                            ops.Sub(_.price, _.mean) / _.stddev * k)), 
-                { 'price' : observable.MidPrice(thisBook),
-                  'mean'  : observable.EWMA(_.price, alpha), 
-                  'stddev': observable.StdDevEW(_.price, alpha) })
+
+import _wrap
+
+class Bollinger_linear(types.ISingleAssetStrategy):
+
+    def getDefinitions(self):
+        return  { 'price' : observable.MidPrice(orderbook.OfTrader()),
+                  'mean'  : observable.EWMA(_.price, self.alpha), 
+                  'stddev': observable.StdDevEW(_.price, self.alpha) }
+            
+    def getImpl(self):
+        return DesiredPosition(
+                        orderFactory = self.orderFactory, 
+                        desiredPosition = ops.Sub(_.price, _.mean) / _.stddev * self.k)
+        
+_wrap.strategy(Bollinger_linear, ['Desired position', 'Bollinger linear'], 
+               """
+               """, 
+               [
+                  ('alpha',        '0.15',                'non_negative'), 
+                  ('k',            'ops.constant(+0.5)',  'IFunction[float]'), 
+                  ('orderFactory', 'order.MarketFactory', 'Side -> Volume -> IOrder'),
+               ], globals())
