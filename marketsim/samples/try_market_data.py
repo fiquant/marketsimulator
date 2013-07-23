807048a82bdf2d8aa3d24b8ef7910617ad4a0659
Charles Podkanski <charles@podkanski.com>
2013-07-23 11:54:34 +0200

    MarketData sample

--- marketsim/samples/try_market_data.py
+++ marketsim/samples/try_market_data.py
@@ -1,13 +1,20 @@
 import sys
 sys.path.append(r'../..')
 
-from marketsim import strategy
+from marketsim import strategy, ops
 from common import expose
 
 @expose("Market Data", __name__, only_veusz=True)
 def MarketData(ctx):
     return [
+        ctx.makeTrader_A(
+            strategy.LiquidityProvider(defaultValue=50.,
+                                       volumeDistr=ops.constant(50)),
+            "LiquidityProvider_A"),
+
         ctx.makeTrader_A(strategy.MarketData(), "marketdata"),
+
+        ctx.makeTrader_A(strategy.MarketMaker(), "marketmaker"),
 
         ctx.makeTrader_A(strategy.Noise(), "noise")
     ]
