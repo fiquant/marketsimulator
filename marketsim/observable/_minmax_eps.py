f3ad1a0bc0cef8bef76043a6ff9a0fc1d6945504
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-27 17:53:38 +0200

    Min/Max eps refactored

--- marketsim/observable/_minmax_eps.py
+++ marketsim/observable/_minmax_eps.py
@@ -1,6 +1,6 @@
 from marketsim import types, event, _, ops
 
-class MaxEpsilon(ops.Observable[float]):
+class Base(ops.Observable[float]):
     """ Observable that fires if underlying source value becomes greater previous maximum plus some epsilon
     """
     
@@ -16,7 +16,7 @@ class MaxEpsilon(ops.Observable[float]):
         self.value = self.source()
         self._handler = _(self)._fire
         if self.value is not None:
-            self._handler = event.GreaterThan(self.value + self.epsilon(), self._handler)
+            self._handler = self._predicate(self.value + self._sign*self.epsilon(), self._handler)
         self.source += self._handler
         
     def bind(self, ctx):
@@ -33,48 +33,25 @@ class MaxEpsilon(ops.Observable[float]):
         
     @property
     def label(self):
-        return "Max^{" + self.source.label + "}_{\epsilon}"
+        return self._label + "^{" + self.source.label + "}_{\epsilon}"
     
     @property
     def attributes(self):
         return {}
+
+
+class MaxEpsilon(Base):
+    """ Observable that fires if underlying source value becomes greater previous maximum plus some epsilon
+    """
+    _sign = +1
+    _predicate = event.GreaterThan
+    _label = "Max"
         
-class MinEpsilon(ops.Observable[float]):
+class MinEpsilon(Base):
     """ Observable that fires if underlying source value becomes less than previous minimum minus some epsilon
     """
     
-    def __init__(self, source, epsilon):
-        ops.Observable[float].__init__(self)
-        self.source = source
-        self.epsilon = epsilon
-        
-    _properties = { 'source' : types.IObservable[float], 
-                    'epsilon': types.IFunction[float] }
-        
-    def _subscribe(self):
-        self.value = self.source()
-        self._handler = _(self)._fire
-        if self.value is not None:
-            self._handler = event.LessThan(self.value - self.epsilon(), self._handler)
-        self.source += self._handler
-        
-    def bind(self, ctx):
-        self._subscribe()
-        
-    def __call__(self):
-        return self.value
-        
-    def _fire(self, dummy):
-        if self.source() is not None:
-            self.source -= self._handler
-            self._subscribe()
-            self.fire(dummy)
-        
-    @property
-    def label(self):
-        return "Min^{" + self.source.label + "}_{\epsilon}"
-    
-    @property
-    def attributes(self):
-        return {}
+    _sign = -1
+    _predicate = event.LessThan
+    _label = "Min"
         
