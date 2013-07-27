cc53f18c049f9d879f8ea4db0fe3f09c9c7aee97
antonkolotaev <anton.kolotaev@gmail.com>
2013-07-27 16:17:48 +0200

    observable.MinEpsilon added

--- marketsim/order/_always_best.py
+++ marketsim/order/_always_best.py
@@ -43,6 +43,44 @@ class MaxEpsilon(ops.Observable[float]):
     def attributes(self):
         return {}
         
+class MinEpsilon(ops.Observable[float]):
+    """ Observable that fires if underlying source value becomes less than previous minimum minus some epsilon
+    """
+    
+    def __init__(self, source, epsilon):
+        ops.Observable[float].__init__(self)
+        self.source = source
+        self.epsilon = epsilon
+        
+    _properties = { 'source' : types.IObservable[float], 
+                    'epsilon': types.IFunction[float] }
+        
+    def _subscribe(self):
+        self.value = self.source()
+        self._handler = _(self)._fire
+        if self.value is not None:
+            self._handler = event.LessThan(self.value - self.epsilon(), self._handler)
+        self.source += self._handler
+        
+    def bind(self, ctx):
+        self._subscribe()
+        
+    def __call__(self):
+        return self.value
+        
+    def _fire(self, dummy):
+        if self.source() is not None:
+            self.source -= self._handler
+            self._subscribe()
+            self.fire(dummy)
+        
+    @property
+    def label(self):
+        return "Min^{" + self.source.label + "}_{\epsilon}"
+    
+    @property
+    def attributes(self):
+        return {}
         
 
 class AlwaysBest(Base):
--- marketsim/samples/common.py
+++ marketsim/samples/common.py
@@ -132,7 +132,7 @@ class Context(object):
     def makeTrader_B(self, strategy, label, additional_ts = []):
         return self.makeTrader(self.book_B, strategy, label, additional_ts)
 
-from marketsim.order._always_best import MaxEpsilon
+from marketsim.order._always_best import MaxEpsilon, MinEpsilon
     
 def orderBooksToRender(ctx, traders):
         books = list(set(itertools.chain(*[t.orderBooks for t in traders]))) 
@@ -161,6 +161,7 @@ def orderBooksToRender(ctx, traders):
             max = observable.Max(assetPrice, 100)
             candlesticks = observable.CandleSticks(assetPrice, 10)
             smax = MaxEpsilon(assetPrice, observable.TickSize(thisBook))
+            smin = MinEpsilon(assetPrice, observable.TickSize(thisBook))
             
             def bollinger(mean, stddev, graph):
                 return [
@@ -199,8 +200,9 @@ def orderBooksToRender(ctx, traders):
 
                 timeserie.ToRecord(assetPrice, ctx.minmax_graph),
                 timeserie.ToRecord(max, ctx.minmax_graph),
-                timeserie.ToRecord(smax, ctx.minmax_graph),
                 timeserie.ToRecord(min, ctx.minmax_graph),
+                timeserie.ToRecord(smax, ctx.minmax_graph),
+                timeserie.ToRecord(smin, ctx.minmax_graph),
             ] 
             + bollinger(ma100, stddev100, ctx.bollinger_100_graph) 
             + bollinger(ma20, stddev20, ctx.bollinger_20_graph)
