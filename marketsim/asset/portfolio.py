b736762d9065bb270f8aeac5c2e3bf4a27bc03a0
Karol Podkanski <charles@podkanski.com>
2013-09-02 16:36:53 +0200

    changes to Portfolio

--- marketsim/asset/portfolio.py
+++ marketsim/asset/portfolio.py
@@ -1,12 +1,31 @@
 
 from asset import *
 
+# TODO: change to take into account more complex positions
+
 class Position(object):
     def __init__(self):
         self._amount = 0
         self._pending = 0
 
 
-class Portfolio(dict):
+class PortfolioBase(dict):
+    def __init__(self):
+        super(PortfolioBase, self).__init__()
+
+
+class Portfolio(PortfolioBase):
     def __init__(self):
         super(Portfolio, self).__init__()
+        self._main_cash_account = None
+
+    def get_cash_account(self):
+        return self._main_cash_account
+
+    def set_cash_account(self, cash_account):
+        if isinstance(CashBase, cash_account):
+            self._main_cash_account = cash_account
+        else:
+            raise TypeError("Trying to set non-cash account")
+
+    cash_account = property(get_cash_account, set_cash_account)
