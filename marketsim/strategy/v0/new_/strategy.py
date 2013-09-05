a100b99dc92b981723176307afdc1f840a84d291
Karol Podkanski <charles@podkanski.com>
2013-09-05 14:56:06 +0200

    changed the way that observables are stored by a strategy
    ex: user_strategy.store( custom_label = SomeObservable(), freq = 5)
    adds a custom_label field to the user_strategy instance which contains
    a BufferedSeries object updated every 5 time steps with the data from
    SomeObservable.
    
    If the freq parameter is ommitted, then the BufferedSeries is updated
    as often as SomeObservable.

--- marketsim/strategy/v0/new_/strategy.py
+++ marketsim/strategy/v0/new_/strategy.py
@@ -12,7 +12,9 @@ class StrategyBase(object):
         self.on_order_created = event.Event()
         self.data = {}
 
-    def store(self, observed, label=None, freq=None):
+    def store(self, observed=None, freq=None, **kwargs):
+        if len(kwargs) == 1:
+            label, observed = kwargs.items()[0]
         if freq:
             observed = OnEveryDt(freq, observed)
         label = observed.label if label is None else label
@@ -74,12 +76,13 @@ class StrategyBase(object):
     _internals = []
 
 from marketsim.orderbook._queue import AskLevels, BidLevels
-from marketsim.observable import OLS
+from marketsim.observable import OLS, LastTradeVolume
 
 from itertools import izip_longest
 
 import statsmodels.api as sm
 import numpy as np
+import pandas as pd
 
 class OrderbookStrategy(StrategyBase):
     # TODO: Should a strategy have its own portfolio?
@@ -92,37 +95,98 @@ class OrderbookStrategy(StrategyBase):
         print "subscribing"
         super(OrderbookStrategy, self)._subscribe()
 
-        self._event = event.Every(ops.constant(1.0))
+        self._event = event.Every(ops.constant(20.0))
         event.subscribe(self._event, self.handle_data, self)
 
         book_A, book_B = self.parent.exchange.books()
         self.asset_A, self.asset_B = self.parent.exchange.assets()
 
-        self.store(LastTradePrice(book_A), label="series_A", freq=1)
-        self.store(LastTradePrice(book_B), label="series_B", freq=1)
-        self.store(PortfolioValue(self.trader), label="portfolio_value", freq=1)
+        self.store(series_A=LastTradePrice(book_A), freq=1)
+        self.store(series_B=LastTradePrice(book_B), freq=1)
+        self.store(value=PortfolioValue(self.trader), freq=1)
+        self.store(volumes=LastTradeVolume(book_A))
 
-        self.ask_levels = AskLevels(book_A, depth=5)
-        self.bid_levels = BidLevels(book_A, depth=5)
-        self._internals.append('ask_levels')
-        self._internals.append('bid_levels')
+        #self.ask_levels = AskLevels(book_A, depth=5)
+        # self.bid_levels = BidLevels(book_A, depth=5)
+        #self._internals.append('ask_levels')
+        # self._internals.append('bid_levels')
 
-        # event.subscribe(book_A.asks.changed, self.qchange, self)
+
 
     def handle_data(self, caller):
         if self.running:
-            print OLS(self.series_A(), window=20)
+            print self.volumes().resample('S', fill_method="bfill", how="sum").plot(linestyle='steps')
+            plt.show()
 
     def qchange(self, caller):
-        #print "orderbook"
-
-        # print self._scheduler.currentTime
-
         levels = izip_longest(self.bid_levels(), self.ask_levels(), fillvalue=(0, 0))
-
         print "orderbook"
         for bid, ask in levels:
             print "{0[0]:6.2f}{0[1]:6d}\t{1[0]:6.2f}{1[1]:6d}".format(bid, ask)
 
 
+import matplotlib.pyplot as plt
+from marketsim.observable import Candlesticks
+import talib as ta
 
+class TrendFollow(StrategyBase):
+    def __init__(self):
+        StrategyBase.__init__(self)
+
+    def _subscribe(self):
+        self._event = event.Every(ops.constant(60.0))
+        event.subscribe(self._event, self.handle_data, self)
+
+        book_A, book_B = self.parent.exchange.books()
+        self.asset_A, self.asset_B = self.parent.exchange.assets()
+        self.store(price=LastTradePrice(book_A))
+        self.store(volume=LastTradeVolume(book_A))
+        self.value = PortfolioValue(self.parent)
+        self.invested = False
+        self.position = Position({self.asset_A: 2})
+
+    def handle_data(self, caller):
+        if not self.invested:
+            self.check_enter_conditions()
+        else:
+            self.check_exit_conditions()
+
+    def check_enter_conditions(self):
+        params = OLS(self.price(), window=20)
+        #pd.DataFrame(Candlesticks(self.price())).plot(linestyle='steps')
+        #plt.show()
+        plt.plot(ta.abstract.Function('sma')(Candlesticks(self.price())))
+        plt.show()
+        if params is not None:
+            for asset, order in self.position.enter.iteritems():
+                self.parent.send(order, asset)
+            self.invested = True
+
+    def check_exit_conditions(self):
+        for asset, order in self.position.exit.iteritems():
+            self.parent.send(order, asset)
+        self.invested = False
+
+
+class Position(object):
+    def __init__(self, portfolio, executor=None):
+        self.portfolio = portfolio
+        self.executor = executor
+
+    @staticmethod
+    def create_orders(portfolio=None, opposite=False):
+        orders = {}
+        for asset, volume in portfolio.iteritems():
+            volume = -volume if opposite else volume
+            orders[asset] = Market.Buy(volume) if volume > 0 else Market.Sell(-volume)
+        return orders
+
+    @property
+    def enter(self):
+        print "entering"
+        return self.create_orders(portfolio=self.portfolio, opposite=False)
+
+    @property
+    def exit(self):
+        print "exiting"
+        return self.create_orders(portfolio=self.portfolio, opposite=True)
