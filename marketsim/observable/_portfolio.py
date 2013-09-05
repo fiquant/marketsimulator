3ab3c30fc51f73261a1dfde2023410053d5b7d82
Karol Podkanski <charles@podkanski.com>
2013-09-05 14:49:48 +0200

    cleaning the implementation of PortfolioValue observable

--- marketsim/observable/_portfolio.py
+++ marketsim/observable/_portfolio.py
@@ -4,29 +4,25 @@ from marketsim import event, _, types, ops
 from marketsim.asset import StockBase
 
 class PortfolioValue(ops.Observable[float]):
-    def __init__(self, trader):
+    def __init__(self, trader, exchange=None):
         super(PortfolioValue, self).__init__()
         self.trader = trader
+        self.exchange = self.trader.exchange if exchange is None else exchange
 
     def __call__(self):
-        sum = 0
-        for asset, amount in self.portfolio:
-            price = self.rough_price(asset)
-            if price is None:
-                # return None
-                pass
-            else:
-                sum += price
-        return sum
+        return self.price(self.portfolio)
+
+    def price(self, portfolio):
+        return sum([amount*self.rough_price(asset) for asset, amount in portfolio.items()])
 
     @property
     def portfolio(self):
-        return self.trader.portfolio.iteritems()
+        return self.trader.portfolio
 
     def rough_price(self, asset):
         if isinstance(asset, StockBase):
-            last_price = self.trader.exchange[asset].lastTrade()
-            return last_price[0] if last_price else None
+            last_price = self.exchange[asset].lastTrade()
+            return last_price[0] if last_price else 0.0
         else:
             # we suppose that the cash is worth 1 per unit
             return 1.0
