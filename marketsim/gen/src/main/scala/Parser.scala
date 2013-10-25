c39cae31714b252dc7ebb86ac2cc259c3deb0e51
antonkolotaev <anton.kolotaev@gmail.com>
2013-10-25 13:54:34 +0200

    pretty printer for arithmetic expressions

--- marketsim/gen/src/main/scala/Parser.scala
+++ marketsim/gen/src/main/scala/Parser.scala
@@ -1,4 +1,6 @@
 import java.io.PrintWriter
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.util.parsing.combinator._
 import resource._
 
@@ -10,6 +12,43 @@ case class Sub(x: Expr, y: Expr) extends Expr
 case class Mul(x: Expr, y: Expr) extends Expr
 case class Div(x: Expr, y: Expr) extends Expr
 case class Neg(x: Expr) extends Expr
+
+case class Memoize[A,B](f: A => B) extends (A => B) {
+    private val cache = mutable.Map.empty[A, B]
+    def apply(x: A) = cache getOrElseUpdate (x, f(x))
+}
+
+object PrettyPrinter {
+
+    val tab = "   "
+
+    private val tabs: Memoize[Int, String] = Memoize {
+        case 0 => ""
+        case n => tabs(n - 1) + tab
+    }
+
+    def tabify(n : Int, s : String) = List(tabs(n) + s)
+
+    def braces(n : Int, name : String, e : Expr) =
+        tabify(n, name)  :::
+            apply(e, n + 1)
+
+    def braces(n : Int, name : String, e1 : Expr, e2 : Expr) =
+        tabify(n, name)  :::
+            apply(e1, n + 1)   :::
+            apply(e2, n + 1)
+
+    def apply(e : Expr, n : Int = 0) : List[String] = e match {
+        case Var(s) => tabify(n, s)
+        case Const(x) => tabify(n, x.toString)
+        case Neg(x) => braces(n,"Neg", x)
+        case Add(x,y) => braces(n,"Add", x, y)
+        case Sub(x,y) => braces(n,"Sub", x, y)
+        case Mul(x,y) => braces(n,"Mul", x, y)
+        case Div(x,y) => braces(n,"Div", x, y)
+    }
+
+}
 
 object Parser extends JavaTokenParsers {
     def expr : Parser[Expr] = factor ~ rep(("+" | "-") ~ factor) ^^ {
@@ -36,11 +75,14 @@ object Parser extends JavaTokenParsers {
 
     def main(args: Array[String]) {
 
-        for (input <- managed(io.Source.fromFile("arithmetic.in"));
-             output <- managed(new PrintWriter("arithmetic.out")))
+        for (input <- managed(io.Source.fromFile("test/arithmetic.in"));
+             output <- managed(new PrintWriter("test/arithmetic.out")))
         {
             for (line <- input.getLines) {
-                output.println(s"$line -> ${parseAll(expr, line)}")
+                output.println(s"$line ->")
+                val parsed = parseAll(expr, line)
+                val pretty_printed = PrettyPrinter(parsed.get, 3).mkString("\n")
+                output.println(s"$pretty_printed\n\n")
             }
         }
     }
