3ad3180b8a63f6ec5f01f7f956367b12211ede54
antonkolotaev <anton.kolotaev@gmail.com>
2013-10-28 22:30:59 +0100

    pretty printing for conditions

--- marketsim/gen/src/main/scala/PrettyPrinter.scala
+++ marketsim/gen/src/main/scala/PrettyPrinter.scala
@@ -16,14 +16,14 @@ object PrettyPrinter {
         case IfThenElse(_,_,_) => 3
     }
 
-    def symbol(s : BinOpSymbol) = s match {
+    def apply(s : BinOpSymbol) = s match {
         case Mul() => "*"
         case Div() => "/"
         case Add() => "+"
         case Sub() => "-"
     }
 
-    def symbol(s : CondSymbol) = s match {
+    def apply(s : CondSymbol) = s match {
         case Less() => "<"
         case LessEqual() => "<="
         case Greater() => ">"
@@ -54,9 +54,27 @@ object PrettyPrinter {
         case Const(x) => x.toString
         case Var(x) => x
         case Neg(x) => s"-${wrap(x, e)}"
-        case BinOp(s, x,y) => s"${wrap(x, e)} ${symbol(s)} ${wrap(y, e, true)}"
-        case IfThenElse(_,x,y) => s"if xxx then ${wrap(x, e)} else ${wrap(y, e)}"
+        case BinOp(s, x,y) => s"${wrap(x, e)}${apply(s)}${wrap(y, e, true)}"
+        case IfThenElse(c,x,y) => s"if ${apply(c)} then ${wrap(x, e)} else ${wrap(y, e)}"
         case FunCall(name, args) => s"${apply(name.names, ".")}(${apply(args, ",")})"
+    }
+
+    def wrap(x : BooleanExpr, e : And) = x match {
+        case y : Or => s"(${apply(y)})"
+        case y => apply(y)
+    }
+
+    def wrap(x : BooleanExpr, e : Not) = x match {
+        case Condition(_,_,_) => apply(x)
+        case _ => s"(${apply(x)})"
+    }
+
+
+    def apply(e : BooleanExpr) : String = e match {
+        case Condition(s, x, y) => s"${apply(x)}${apply(s)}${apply(y)}"
+        case Or(x, y) => s"${apply(x)} or ${apply(y)}"
+        case a @ And(x, y) => s"${wrap(x, a)} and ${wrap(y, a)}"
+        case a @ Not(x) =>  s"not ${wrap(x, a)}"
     }
 
 }
--- marketsim/gen/test/expressions.pp
+++ marketsim/gen/test/expressions.pp
@@ -1,24 +1,24 @@
 1 + -6*z ->
-1.0 + -6.0 * z
+1.0+-6.0*z
 
 
 -t/p -a*l ->
--t / p - a * l
+-t/p-a*l
 
 
 1 + 3 - (a + b) ->
-1.0 + 3.0 - (a + b)
+1.0+3.0-(a+b)
 
 
 a*v / (a*p) ->
-a * v / (a * p)
+a*v/(a*p)
 
 
 p + (if a + 12 < 9 then n - 2 else if j*u <> 7 or (j >= 0) and not o = i then 8 + j else k) ->
-p + (if xxx then n - 2.0 else if xxx then 8.0 + j else k)
+p+(if a+12.0<9.0 then n-2.0 else if j*u<>7.0 or j>=0.0 and not o=i then 8.0+j else k)
 
 
 a(12, b(p, a + 8)) ->
-a(12.0,b(p,a + 8.0))
+a(12.0,b(p,a+8.0))
 
 
