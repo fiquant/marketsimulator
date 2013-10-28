6f6aa0bcbcc92b6233e6e215ef2db2e41bcf582f
antonkolotaev <anton.kolotaev@gmail.com>
2013-10-28 14:51:54 +0100

    AST.scala added

--- marketsim/gen/src/main/scala/Parser.scala
+++ marketsim/gen/src/main/scala/Parser.scala
@@ -1,45 +1,6 @@
 import scala.collection.mutable
 import scala.util.parsing.combinator._
 
-case class Parameter(name        : String,
-                     ty          : Option[String],
-                     initializer : Option[Expr],
-                     annotations : List[Annotation])
-
-case class QualifiedName(names   : List[String])
-
-case class Annotation(name       : QualifiedName,
-                      parameters : List[String])
-
-case class FunDef(name           : String,
-                  parameters     : List[Parameter],
-                  body           : Option[Expr],
-                  docstring      : Option[String],
-                  annotations    : List[Annotation])
-
-sealed abstract class Expr
-case class Const(value: Double) extends Expr
-case class Var(s : String) extends Expr
-case class Add(x: Expr, y: Expr) extends Expr
-case class Sub(x: Expr, y: Expr) extends Expr
-case class Mul(x: Expr, y: Expr) extends Expr
-case class Div(x: Expr, y: Expr) extends Expr
-case class Neg(x: Expr) extends Expr
-case class IfThenElse(cond : BooleanExpr, x : Expr, y : Expr) extends Expr
-case class FunCall(name : QualifiedName, args : List[Expr]) extends Expr
-
-sealed abstract class BooleanExpr
-case class Less         (x : Expr, y : Expr) extends BooleanExpr
-case class LessEqual    (x : Expr, y : Expr) extends BooleanExpr
-case class Greater      (x : Expr, y : Expr) extends BooleanExpr
-case class GreaterEqual (x : Expr, y : Expr) extends BooleanExpr
-case class Equal        (x : Expr, y : Expr) extends BooleanExpr
-case class NotEqual     (x : Expr, y : Expr) extends BooleanExpr
-
-case class Or   (x : BooleanExpr, y : BooleanExpr) extends BooleanExpr
-case class And  (x : BooleanExpr, y : BooleanExpr) extends BooleanExpr
-case class Not  (x : BooleanExpr) extends BooleanExpr
-
 case class Memoize[A,B](f: A => B) extends (A => B) {
     private val cache = mutable.Map.empty[A, B]
     def apply(x: A) = cache getOrElseUpdate (x, f(x))
