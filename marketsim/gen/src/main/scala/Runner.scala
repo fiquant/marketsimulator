62e3ecc3f31197e12e56238df09bcc8ec1062d73
antonkolotaev <anton.kolotaev@gmail.com>
2013-11-05 14:45:46 +0100

    -

--- marketsim/gen/src/main/scala/Runner.scala
+++ marketsim/gen/src/main/scala/Runner.scala
@@ -1,50 +1,46 @@
 import java.io.PrintWriter
-import PyGen.ImportRandom
 import resource._
 import sext._
 
 object Runner extends Parser {
     
-    def getPythonDefinitions(path : String) : List[PyGen.Printer] = 
-    {
-        var ret : List[PyGen.Printer] = Nil
-        
-        for (input <- managed(io.Source.fromFile(path));
-             raw_output <- managed(new PrintWriter(path.replace(".sc", ".raw")));
-             pp_output <- managed(new PrintWriter(path.replace(".sc", ".pp")));
-             raw_py_output <- managed(new PrintWriter(path.replace(".sc", ".py.raw"))))
-        {
-            val in = input.mkString
-            raw_output.println(s"$in ->")
-            pp_output.println(s"$in ->")
+    def getPythonDefinitions(path : String) : List[PyGen.Printer] =
+        (managed(io.Source.fromFile(path)) and
+         managed(new PrintWriter(path.replace(".sc", ".raw"))) and
+         managed(new PrintWriter(path.replace(".sc", ".pp"))) and
+         managed(new PrintWriter(path.replace(".sc", ".py.raw"))) map {
 
-            val (raw, pp, raw_py, python_definitions) = parseAll(definitions, in) match {
-                case Success(result , _) => {
-                    val pp = result.toString
-                    parseAll(definitions, pp) match {
-                        case Success(result2, _) => {
-                            if (result != result2) {
-                                println(s"input: in")
-                                println(s"parsed: $result")
-                                println(s"re-parsed: $result2")
-                            }
-                        }
-                        case x => println(x)
-                    }
-                    val python = result.python
-                    val randoms = PyGen.getRandoms(python)
-                    (result.treeString, pp, randoms.treeString, randoms)
-                }
-                case x => (x.toString, x.toString, x.toString, List())
-            }
+             case (((input, raw_output), pp_output), raw_py_output) => Nil
+                 val in = input.mkString
+                 raw_output.println(s"$in ->")
+                 pp_output.println(s"$in ->")
 
-            raw_output.println(raw)
-            pp_output.println(pp)
-            raw_py_output.println(raw_py)
-            ret = python_definitions
-        }
-        ret
-    }
+                 val (raw, pp, raw_py, python_definitions) = parseAll(definitions, in) match {
+                     case Success(result, _) => {
+                         val pp = result.toString
+                         parseAll(definitions, pp) match {
+                             case Success(result2, _) => {
+                                 if (result != result2) {
+                                     println(s"input: in")
+                                     println(s"parsed: $result")
+                                     println(s"re-parsed: $result2")
+                                 }
+                             }
+                             case x => println(x)
+                         }
+                         val python = result.python
+                         val randoms = PyGen.getRandoms(python)
+                         (result.treeString, pp, randoms.treeString, randoms)
+                     }
+                     case x => (x.toString, x.toString, x.toString, List())
+                 }
+
+                 raw_output.println(raw)
+                 pp_output.println(pp)
+                 raw_py_output.println(raw_py)
+                 python_definitions
+
+         }).opt.get
 
     def main(args: Array[String]) {
 
@@ -56,8 +52,7 @@ object Runner extends Parser {
 
         for ((filename, definitions) <- python_definitions.groupBy({ _.filename }))
         {
-            for (py_output <- managed(new PrintWriter(filename)))
-            {
+            managed(new PrintWriter(filename)) map { py_output =>
                 py_output.print(definitions.head.prologue)
                 py_output.println(definitions.mkString("\r\n"))
             }
