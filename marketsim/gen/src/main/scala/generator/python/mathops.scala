3c00f1520b88e34825ebca787675d68d8dbd4224
antonkolotaev <anton.kolotaev@gmail.com>
2013-11-22 13:23:38 +0100

    generator/python/mathops.scala added

---
+++ marketsim/gen/src/main/scala/generator/python/mathops.scala
@@ -0,0 +1,12 @@
+package generator.python
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Anton
+ * Date: 22/11/13
+ * Time: 13:20
+ * To change this template use File | Settings | File Templates.
+ */
+class mathops {
+
+}
