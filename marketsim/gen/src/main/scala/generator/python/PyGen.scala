19adccff7e3575214e2b709ea3479afa44bf23b1
antonkolotaev <anton.kolotaev@gmail.com>
2013-11-22 13:23:49 +0100

    generator/python/mathops.scala added

--- marketsim/gen/src/main/scala/generator/python/PyGen.scala
+++ marketsim/gen/src/main/scala/generator/python/PyGen.scala
@@ -16,8 +16,8 @@ package object PyGen {
         override def toString = registry.toString()
 
         // TODO: non-intrusive registration
-        register(generator.python.random)
-        register(ImportMathops)
+        register(random)
+        register(mathops)
     }
 
     abstract class ParameterBase {
@@ -101,55 +101,4 @@ package object PyGen {
         override def toString = s"""$header$doc$init$label$properties$repr"""
     }
 
-    case class ParameterOfMathops(name : String, initializer : Double) extends ParameterBase
-    {
-        val ty = "float"
-        val s_initializer = initializer.toString
-
-        override def assign = s"""self.$name = $name
-        |$tab${tab}if isinstance($name, types.IEvent):
-        |$tab${tab}${tab}event.subscribe(self.$name, self.fire, self)""".stripMargin
-
-        def nullable = s"""$name = self.$name()
-                |$tab${tab}if $name is None: return None""".stripMargin
-    }
-
-    case class ImportMathops(name        : String,
-                             category    : String,
-                             override val impl_function : String,
-                             label_tmpl  : Option[String],
-                             parameters  : List[ParameterOfMathops],
-                             docstring   : String) extends Printer()
-    {
-        type Parameter = ParameterOfMathops
-        val impl_module = "math"
-        val alias = name
-        val filename = "defs/mathops.py"
-
-        override def repr_body = s"""$tab${tab}return "$label_tmpl" % self.__dict__"""
-
-        override val base_class = "Observable[float]"
-
-        override def init_body = s"""$tab${tab}Observable[float].__init__(self)
-        |${super.init_body}""".stripMargin
-
-        def nullable_fields = join_fields({ _.nullable}, crlf + tab + tab)
-
-        override def call_body = s"$tab$tab$nullable_fields" + crlf + super.call_body
-
-        override def toString = super.toString + s"""$call"""
-
-        val prologue =
-            """
-              |from marketsim import registry, types, event
-              |import math
-              |from _all import Observable, Constant
-            """.stripMargin
-    }
-
-    object ImportMathops extends Typed.AnnotationHandler
-    {
-        def apply(f : Typed.Function) = ("","",Nil)
-        val name = "python.mathops"
-    }
 }
--- marketsim/gen/src/main/scala/generator/python/mathops.scala
+++ marketsim/gen/src/main/scala/generator/python/mathops.scala
@@ -1,12 +1,55 @@
 package generator.python
 
-/**
- * Created with IntelliJ IDEA.
- * User: Anton
- * Date: 22/11/13
- * Time: 13:20
- * To change this template use File | Settings | File Templates.
- */
-class mathops {
+object mathops extends Typed.AnnotationHandler
+{
+    import PyGen.{tab, crlf}
 
+    case class ParameterOfMathops(name : String, initializer : Double) extends PyGen.ParameterBase
+    {
+        val ty = "float"
+        val s_initializer = initializer.toString
+
+        override def assign = s"""self.$name = $name
+        |$tab${tab}if isinstance($name, types.IEvent):
+        |$tab${tab}${tab}event.subscribe(self.$name, self.fire, self)""".stripMargin
+
+        def nullable = s"""$name = self.$name()
+                |$tab${tab}if $name is None: return None""".stripMargin
+    }
+
+    case class ImportMathops(name        : String,
+                             category    : String,
+                             override val impl_function : String,
+                             label_tmpl  : Option[String],
+                             parameters  : List[ParameterOfMathops],
+                             docstring   : String) extends PyGen.Printer()
+    {
+        type Parameter = ParameterOfMathops
+        val impl_module = "math"
+        val alias = name
+        val filename = "defs/mathops.py"
+
+        override def repr_body = s"""$tab${tab}return "$label_tmpl" % self.__dict__"""
+
+        override val base_class = "Observable[float]"
+
+        override def init_body = s"""$tab${tab}Observable[float].__init__(self)
+        |${super.init_body}""".stripMargin
+
+        def nullable_fields = join_fields({ _.nullable}, crlf + tab + tab)
+
+        override def call_body = s"$tab$tab$nullable_fields" + crlf + super.call_body
+
+        override def toString = super.toString + s"""$call"""
+
+        val prologue =
+            """
+              |from marketsim import registry, types, event
+              |import math
+              |from _all import Observable, Constant
+            """.stripMargin
+    }
+
+    def apply(f : Typed.Function) = ("","",Nil)
+    val name = "python.mathops"
 }
