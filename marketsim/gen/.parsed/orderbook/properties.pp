2052fdfb45345072093c590d5cb5c3d72a3538a3
antonkolotaev <anton.kolotaev@gmail.com>
2014-01-23 18:00:15 +0100

    splitting orderbook.sc and trader.sc

--- marketsim/gen/.parsed/orderbook/properties.pp
+++ marketsim/gen/.parsed/orderbook/properties.pp
@@ -1,56 +1,6 @@
 @category = "Asset"
 
 package orderbook() {
-    /** Order book for a single asset in a market.
-     * Maintains two order queues for orders of different sides
-     */
-    @python.intrinsic("orderbook.local._Local_Impl")
-    @label = "%(name)s"
-    def Local(tickSize = 0.01,
-              _digitsToShow = 2,
-              name = "-orderbook-",
-              timeseries = [] : List[ITimeSerie]) : IOrderBook
-        
-    
-    /** Represent an *orderbook* from point of view of a remote trader connected
-     * to the market by means of a *link* that introduces some latency in information propagation
-     */
-    @python.intrinsic("orderbook.remote._Remote_Impl")
-    @label = "%(orderbook)s.name^remote"
-    def Remote(orderbook = Local(),
-               link = TwoWayLink(),
-               timeseries = [] : List[ITimeSerie]) : IOrderBook
-        
-    
-    /** Represents latency in information propagation from one agent to another one
-     * (normally between a trader and a market).
-     * Ensures that sending packets via a link preserves their order.
-     */
-    @python.intrinsic("orderbook.link._Link_Impl")
-    def Link(/** function called for each packet in order to determine
-               * when it will appear at the end point*/ latency = const(0.001)) : ILink
-        
-    
-    /** Represents latency in information propagation between two agents
-     * (normally between a trader and a market).
-     * Ensures that sending packets via links preserves their order.
-     * Holds two one-way links in opposite directions.
-     */
-    @python.intrinsic("orderbook.link._TwoWayLink_Impl")
-    def TwoWayLink(/** Forward link (normally from a trader to a market)*/ up = Link(),
-                   /** Backward link (normally from a market to a trader)*/ down = Link()) : ITwoWayLink
-        
-    
-    @python.intrinsic("orderbook.of_trader._OfTrader_Impl")
-    @label = "N/A"
-    def OfTrader(Trader = trader.SingleProxy() : IAccount) : IOrderBook
-        
-    
-    @python.intrinsic("orderbook.of_trader._Proxy_Impl")
-    @label = "N/A"
-    def Proxy() : IOrderBook
-        
-    
     @python.intrinsic("orderbook.proxy._Queue_Impl")
     def Queue(book = OfTrader(),
               side = side.Sell()) : IOrderQueue
---
+++ marketsim/gen/defs/trader/properties.sc
@@ -0,0 +1,23 @@
+@category = "Trader"
+package trader
+{
+    @python.intrinsic("trader.props.Position_Impl")
+    def Position(trader =SingleProxy() : IAccount) : IObservable[Volume]
+
+    @python.intrinsic("trader.props.Balance_Impl")
+    def Balance(trader = SingleProxy() : IAccount) : IObservable[Price]
+
+    @python.intrinsic("trader.props.PendingVolume_Impl")
+    def PendingVolume(trader = SingleProxy() : IAccount) : IObservable[Volume]
+
+    def Efficiency(trader = SingleProxy() : IAccount)
+        = observable.Float(Balance(trader) + orderbook.CumulativePrice(orderbook.OfTrader(trader), Position(trader)))
+
+    def RoughPnL(trader = SingleProxy() : IAccount)
+        = observable.Float(Balance(trader) + orderbook.NaiveCumulativePrice(orderbook.OfTrader(trader), Position(trader)))
+
+    def EfficiencyTrend(trader = SingleProxy() : IAccount, alpha = 0.15)
+        = math.Derivative(math.EW.Avg(Efficiency(trader), alpha))
+}
+
+
