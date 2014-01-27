49243447f5aa80bde4a46fe566cae6641725bc9e
antonkolotaev <anton.kolotaev@gmail.com>
2014-01-27 18:14:10 +0100

    removing old files

--- marketsim/observable/__init__.py
+++ marketsim/observable/__init__.py
@@ -1,11 +1,3 @@
 from _computed import OnEveryDt, MultiFold, UpdatableLookback, aggregate, IndicatorBase, IndicatorBaseT
-                        
-from _average import Fold
-
-from _orderbook import (VolumeLevels, TickSize,
-                        QueuePrice, AskLastTradePrice, BidLastTradePrice,
-                        QueueLastPrice, AskLastPrice, BidLastPrice,
-                        BidPrice, AskPrice, MidPrice, Spread,
-                        LastTradePrice, AskWeightedPrice, BidWeightedPrice)
 
 from _trader import  InstEfficiency, PnL, VolumeTraded, PendingVolume, OnOrderMatched
