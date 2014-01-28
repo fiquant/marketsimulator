f6322172de736aa8d5f18ee65c45fda95786fdbd
antonkolotaev <anton.kolotaev@gmail.com>
2014-01-28 14:44:53 +0100

    removing old files

--- marketsim/ops/__init__.py
+++ marketsim/ops/__init__.py
@@ -5,13 +5,4 @@ from _all import (_None, Observable, constant,
                   Less, Greater, LessEqual, GreaterEqual, Equal, NotEqual,
                   less, greater, less_equal, greater_equal, equal, notequal)
 
-from _derivative import Derivative
-
 from _arithmetic import Sub, Sum, Div, Product
-
-from marketsim.gen._out.math._Atan import Atan
-from marketsim.gen._out.math._Exp import Exp
-from marketsim.gen._out.math._Log import Log
-from marketsim.gen._out.math._Pow import Pow
-from marketsim.gen._out.math._Sqrt import Sqrt
-from marketsim.gen._out.math._Sqr import Sqr
