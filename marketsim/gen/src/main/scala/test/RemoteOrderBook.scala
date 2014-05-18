8999e1b63dab75b062e16af8ebf9cec1ebcd93cf
antonkolotaev <anton.kolotaev@gmail.com>
2014-05-18 11:58:57 +0200

    LoggedAccount

--- marketsim/gen/src/main/scala/test/RemoteOrderBook.scala
+++ marketsim/gen/src/main/scala/test/RemoteOrderBook.scala
@@ -2,7 +2,6 @@ package test
 
 import marketsim._
 import marketsim.Scheduler._
-import scala.collection.immutable.Queue
 
 case object RemoteOrderBook extends Test {
 
@@ -16,42 +15,16 @@ case object RemoteOrderBook extends Test {
             val link = new marketsim.orderbook.remote.TwoWayLink(() => 0.2, () => 0.3)
             val book = withLogging(trace)(new marketsim.orderbook.remote.Book(local, link))
 
-            val account = new marketsim.Account(book)
-
-            account.OrderSent += { order => trace("Sending " + order) }
-            account.OrderTraded += { case (order, price, volume)  =>
-                trace(s"$order traded $volume at $price")
-                trace(s"position = ${account.getPosition}; balance = ${account.getBalance}")
-            }
-            account.OrderStopped += { case (order, unmatched) =>
-                trace(order + (if (unmatched == 0) " matched completely" else " unmatched volume: " + unmatched )) }
-
-            var ordersSent = Queue.empty[LimitOrder]
-
-            def sendLimit(factory : LimitOrderFactory) {
-                trace("before = " + book)
-                val order = factory.create
-                account send order
-                ordersSent = ordersSent enqueue order
-                async {
-                    trace("after = " + book)
-                    trace("")
-                }
-            }
+            val account = new LoggedAccount(trace, book)
 
             val sellOrders = LimitOrderFactory(() => 100 + currentTime.toInt, () => 1)
             val buyOrders = LimitOrderFactory(() => 95 + currentTime.toInt, () => -2)
 
-            0 to 4 foreach { i => schedule(i, sendLimit(sellOrders)) }
-            0 to 4 foreach { i => schedule(i + 5, sendLimit(buyOrders)) }
-
-            def cancel(order : LimitOrder) {
-                trace("cancelling " + order)
-                account send CancelOrder(order)
-            }
+            0 to 4 foreach { i => schedule(i,     account sendOrder sellOrders) }
+            0 to 4 foreach { i => schedule(i + 5, account sendOrder buyOrders) }
 
             schedule(10, {
-                ordersSent foreach cancel
+                account.ordersSent.getOrders foreach account.cancel
             })
 
             scheduler workTill 50
