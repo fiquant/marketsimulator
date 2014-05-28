308e5e361658f796223981376525ec5821646160
antonkolotaev <anton.kolotaev@gmail.com>
2014-05-28 10:32:20 +0200

    at with stop loss order best price listener is reinserted only if price changed

--- marketsim/gen/src/main/scala/marketsim/order/WithStopLoss.scala
+++ marketsim/gen/src/main/scala/marketsim/order/WithStopLoss.scala
@@ -18,35 +18,51 @@ object WithStopLoss
                 {
                     var volumeUnmatched = proto.volume
                     var priceTraded = 0
-                    var stopOrder = Option.empty[StopLoss.Order]
+                    var stopOrder = Option.empty[marketsim.Order]
 
-                    val source = proto.side match {
-                        case Sell => OnceGreaterThan(orderbook.BestPrice(target.Bids))
-                        case Buy  => OnceLessThan(orderbook.BestPrice(target.Asks))
+                    var cancelled = false
+                    var handler = Option.empty[(Ticks, () => Unit)]
+
+                    def flush()
+                    {
+                        if (!cancelled) {
+                            target handle CancelOrder(proto)
+                            stopOrder = Some(MarketOrder(-volumeMatched))
+                            target handle OrderRequest(stopOrder.get, this)
+                            cancel_ = () => {
+                                target handle CancelOrder(stopOrder.get)
+                                bestPriceChanged -= handler.get
+                            }
+                        }
                     }
-                    
+
+                    val bestPriceChanged = proto.side match {
+                        case Buy => OnceLessThan(orderbook.BestPrice(target.Bids))
+                        case Sell  => OnceGreaterThan(orderbook.BestPrice(target.Asks))
+                    }
+
                     def volumeMatched = proto.volume - volumeUnmatched
                     def perSharePrice = (priceTraded / volumeMatched).abs
 
                     def OnTraded(o : marketsim.Order, price : Ticks, volume : Int)
                     {
                         volumeUnmatched += volume
-                        priceTraded += price
 
-                        if (volumeMatched != 0)
+                        if (volumeMatched != 0 && priceTraded != price && Some(o) != stopOrder)
                         {
-                            if (stopOrder.nonEmpty)
-                                target handle CancelOrder(stopOrder.get)
+                            if (handler.nonEmpty)
+                                bestPriceChanged -= handler.get
 
                             val trigger = o.side match {
                                 case Buy => perSharePrice * (1 - lossFactor)
                                 case Sell => perSharePrice / (1 - lossFactor)
                             }
 
-                            stopOrder = Some(StopLoss.Order(MarketOrder(-volumeMatched), trigger.toInt))
+                            handler = Some((trigger.toInt, flush))
 
-                            target handle OrderRequest(stopOrder.get, events proxy self)
+                            bestPriceChanged += handler.get
                         }
+                        priceTraded += price
 
                         events OnTraded (self, price, volume)
                     }
@@ -54,12 +70,17 @@ object WithStopLoss
                     def OnStopped(o : marketsim.Order, unmatched : Int)
                     {
                         self.cancel_ = () => ()
+
+                        if (Some(o) == stopOrder)
+                            events OnStopped (self, volumeUnmatched)
                     }
 
                     target handle OrderRequest(proto, this)
 
-                    self.cancel_ = () =>
+                    self.cancel_ = () => {
                         target handle CancelOrder(proto)
+                        cancelled = true
+                    }
                 }
 
                 new State
